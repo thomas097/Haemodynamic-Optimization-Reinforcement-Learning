58563c03197fa37ad1cf8401b2096ffc63c0bc54
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-10-05 15:03:43 +0200

    Added gradient clipping to improve stability

--- experiments/train_DQN_with_CKCNN_encoder.py
+++ experiments/train_DQN_with_CKCNN_encoder.py
@@ -11,9 +11,10 @@ import copy
 import pandas as pd
 
 from tqdm import tqdm
-from DQN import DuelingDQN
+from DQN import DuelingDQN, weighted_Huber_loss
 from ckconv_v3 import CKConv
-from ExperienceReplay import PrioritizedExperienceReplay#, OrderedExperienceReplay
+from ExperienceReplay import PrioritizedExperienceReplay
+from PerformanceTracking import PerformanceTracker
 
 
 # Temporary encoder model
@@ -32,17 +33,18 @@ class CKCNN(torch.nn.Module):
 
 # Modified `fit_dueling_double_dqn()` returning whole histories
 def fit_dueling_double_DQN_with_history(model, encoder, dataset, state_cols, action_col, reward_col, episode_col,
-                                        timestep_col, num_episodes=1, alpha=1e-3, gamma=0.99, tau=1e-2, eval_after=100,
-                                        batch_size=32, replay_alpha=0.0, replay_beta0=0.4, scheduler_gamma=0.9,
-                                        step_scheduler_after=100, freeze_encoder=False):
+                                        timestep_col, num_episodes=1, alpha=1e-3, gamma=0.99, tau=1e-2, eval_func=None,
+                                        eval_after=100, batch_size=32, replay_alpha=0.0, replay_beta0=0.4,
+                                        scheduler_gamma=0.9, step_scheduler_after=100, freeze_encoder=False):
     # Load full dataset into buffer
     replay_buffer = PrioritizedExperienceReplay(dataset, state_cols, action_col, reward_col, episode_col, timestep_col,
                                                 alpha=replay_alpha, beta0=replay_beta0, return_history=True)
 
-    # Set Adam optimizer with Stepwise lr schedule and loss
+    tracker = PerformanceTracker()
+
+    # Set Adam optimizer with Stepwise lr schedule
     optimizer = torch.optim.Adam(model.parameters(), lr=alpha)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=scheduler_gamma)
-    huber_loss = torch.nn.HuberLoss()
 
     # Copy of model for stabilization
     target = copy.deepcopy(model)
@@ -52,6 +54,10 @@ def fit_dueling_double_DQN_with_history(model, encoder, dataset, state_cols, act
     if freeze_encoder:
         for param in encoder.parameters():
             param.requires_grad = False
+
+    # Gradient clipping
+    for w in model.parameters():
+        w.register_hook(lambda grad: torch.clamp(grad, -1, 1))
 
     for ep in tqdm(range(num_episodes)):
         model.train(True)
@@ -73,34 +79,49 @@ def fit_dueling_double_DQN_with_history(model, encoder, dataset, state_cols, act
             q_target = rewards + gamma * target(next_states).gather(dim=1, index=next_model_action)
 
         # Q-estimates of model for states
-        q_prev = model(states).gather(dim=1, index=actions)
+        model_q_values = model(states)
+        q_prev = model_q_values.gather(dim=1, index=actions)
 
         # Estimate loss
-        # TODO: add importance weight
-        loss = huber_loss(q_prev, q_target)
+        loss = weighted_Huber_loss(q_prev, q_target, imp_weights)
 
         # Aggregate loss
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        # TODO: update importance weight
-
         # Update target network
         with torch.no_grad():
             for target_w, model_w in zip(target.parameters(), model.parameters()):
                 target_w.data = ((1 - tau) * target_w.data + tau * model_w.data).clone()
 
-        # Update lr scheduler
+        # Update lr scheduler every few episodes
         if ep % step_scheduler_after == 0 and ep > 0:
             scheduler.step()
 
-        ########################
-        #      Evaluation      #
-        ########################
+        # TD-error
+        td_error = torch.abs(q_target - q_prev)
+        # TODO: update importance weights
+
+        #######################
+        #       Metrics       #
+        #######################
+
+        tracker.add('huber_loss', loss.item())
+        tracker.add('avg_Q_value', torch.mean(model_q_values).item())
+        tracker.add('avg_best_Q_value', torch.mean(torch.max(model_q_values, dim=1)[0]).item())
+        tracker.add('abs_TD_error', torch.mean(td_error))
 
         if ep % eval_after == 0:
-            print('\nEp %s/%s: HuberLoss = %.2f' % (ep, num_episodes, loss.item()))
+
+            # If simulator available, validate model over 100 episodes
+            if eval_func:
+                avg_reward = eval_func(model).groupby(episode_col)[reward_col].sum().mean()
+                tracker.add('avg_reward', avg_reward)
+
+            print('\nEp %s/%s: %s' % (ep, num_episodes, tracker.print_stats()))
+
+        tracker.save()
 
 
 if __name__ == '__main__':
@@ -122,7 +143,6 @@ if __name__ == '__main__':
 
     # load training data
     df_train = pd.read_csv('../preprocessing/datasets/mimic-iii/handcrafted/mimic-iii_train_handcrafted.csv', index_col=0)
-    df_train = df_train.fillna(0.0)  # TODO: remove
 
     # create encoder
     encoder_model = CKCNN(in_channels=len(STATE_SPACE_FEATURES), out_channels=LATENT_STATE_DIM)
