0cb022a341a12362910daa0f18e44e1fbc2f576f
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-10-07 00:11:31 +0200

    renaming

---
+++ model/DQN/experience_replay.py
@@ -0,0 +1,185 @@
+import pandas as pd
+import numpy as np
+import torch
+from torch.nn.utils.rnn import pad_sequence
+
+
+class ExperienceReplay:
+    """ Implements a simple Experience Replay buffer which returns
+        episodes in the order they occur in the dataset.
+    """
+    def __init__(self, dataset, state_cols, action_col='action', reward_col='reward', episode_col='episode',
+                 timestep_col='timestep', return_history=False):
+        self._df = dataset.reset_index()
+        self._state_cols = state_cols
+        self._action_col = action_col
+        self._reward_col = reward_col
+        self._episode_col = episode_col
+        self._timestep_col = timestep_col
+
+        # Determines indices of non-terminal states in df
+        self._indices = self._non_terminal_indices(self._df)
+        self._buffer_size = len(self._indices)
+        self._return_history = return_history
+
+    def _non_terminal_indices(self, df):
+        # Extract indices of all non-terminal states
+        indices = []
+        for _, episode in df.groupby(self._episode_col):
+            indices += list(episode['index'].values[:-1])  # [:-1] ensures terminal states are not sampled
+        return indices
+
+    @staticmethod
+    def _consolidate_length(histories):
+        histories = [torch.Tensor(seq) for seq in histories]
+        return pad_sequence(histories, batch_first=True, padding_value=0.0)
+
+    def batches(self, N):
+        # Generator which yields batches of size N
+        for j in range(0, self._buffer_size, N):
+
+            batch_indices = self._indices[j: j + N]
+            states, actions, rewards, next_states = [], [], [], []
+
+            for i in batch_indices:
+
+                if self._return_history:
+                    # What episode does transition `i` belong to?
+                    ep = self._df.loc[i][self._episode_col]
+
+                    # Extract relevant history of transition `i` and next state
+                    episode = self._df[self._df[self._episode_col] == ep]
+                    history = episode[episode['index'] <= i + 1][self._state_cols].values
+                else:
+                    # Extract just current and next state
+                    history = self._df.loc[i: i + 1][self._state_cols].values
+
+                states.append(history[:-1])
+                actions.append(self._df.loc[i][self._action_col])
+                rewards.append(self._df.loc[i][self._reward_col])
+                next_states.append(history[1:])
+
+            # If return_histories, consolidate their lengths
+            if self._return_history:
+                states = self._consolidate_length(states)
+                next_states = self._consolidate_length(next_states)
+            else:
+                states = torch.Tensor(np.array(states))[:, 0]  # If no history, no need to keep track of temporal dimension
+                next_states = torch.Tensor(np.array(next_states))[:, 0]
+
+            # convert to torch Tensors
+            actions = torch.LongTensor(actions).unsqueeze(1)  # TODO: enable gpu devices!
+            rewards = torch.Tensor(rewards).unsqueeze(1)
+
+            yield states, actions, rewards, next_states
+
+
+class PrioritizedExperienceReplay:
+    """ Implements the Prioritized Experience Replay (PER) buffer
+        (Schaul et al., 2016) for off-policy RL training from log-data.
+        See: https://arxiv.org/pdf/1511.05952v3.pdf
+    """
+    def __init__(self, dataset, state_cols, action_col='action', reward_col='reward', episode_col='episode',
+                 timestep_col='timestep', alpha=0.6, beta0=0.4, return_history=False):
+        self._df = dataset.reset_index()
+        self._state_cols = state_cols
+        self._action_col = action_col
+        self._reward_col = reward_col
+        self._episode_col = episode_col
+        self._timestep_col = timestep_col
+
+        # Determines indices of non-terminal states in df
+        self._indices = self._non_terminal_indices(self._df)
+
+        self._buffer_size = len(self._indices)
+        self._TD_errors = np.ones(self._buffer_size) * 1e16  # Big numbers ensure all are sampled
+
+        self._alpha = alpha  # 0.0 is equiv. to uniform sampling
+        self._beta0 = beta0
+        self._return_history = return_history
+
+    def _non_terminal_indices(self, df):
+        # Extract indices of all non-terminal states
+        indices = []
+        for _, episode in df.groupby(self._episode_col):
+            indices += list(episode['index'].values[:-1])  # [:-1] ensures terminal states are not sampled
+        return np.array(indices)
+
+    def _selection_probs(self):
+        z = self._TD_errors ** self._alpha
+        return z / np.sum(z)
+
+    def _importance_weights(self, chosen_indices, selection_probs):
+        idx = [np.where(self._indices == i)[0][0] for i in chosen_indices]  # Indices in full index
+        w = (self._buffer_size * selection_probs[idx]) ** -self._beta0
+        return w / np.max(w)
+
+    def update(self, indices, td_errors):
+        idx = [np.where(self._indices == i)[0][0] for i in indices]  # Indices in full index
+        self._TD_errors[idx] = np.absolute(td_errors)
+
+    @staticmethod
+    def _consolidate_length(histories):
+        histories = [torch.Tensor(seq) for seq in histories]
+        return pad_sequence(histories, batch_first=True, padding_value=0.0)
+
+    def sample(self, N):
+        # Stochastically sampling of transitions (indices) from replay buffer
+        probs = self._selection_probs()
+        trans_indices = np.random.choice(self._indices, size=N, replace=False, p=probs)
+
+        # Compute importance sampling weights for Q-table update
+        imp_weights = self._importance_weights(trans_indices, probs)
+
+        states = []
+        actions = []
+        rewards = []
+        next_states = []
+
+        for i in trans_indices:
+
+            if self._return_history:
+                # What episode does transition `i` belong to?
+                ep = self._df.loc[i][self._episode_col]
+
+                # Extract relevant history of transition `i` and next state
+                episode = self._df[self._df[self._episode_col] == ep]
+                history = episode[episode['index'] <= i + 1][self._state_cols].values
+            else:
+                # Extract just current and next state
+                history = self._df.loc[i: i + 1][self._state_cols].values
+
+            states.append(history[:-1])
+            actions.append(self._df.loc[i][self._action_col])
+            rewards.append(self._df.loc[i][self._reward_col])
+            next_states.append(history[1:])
+
+        # If return_histories, consolidate their lengths
+        if self._return_history:
+            states = self._consolidate_length(states)
+            next_states = self._consolidate_length(next_states)
+        else:
+            states = torch.Tensor(np.array(states))[:, 0]  # If no history, no need to keep track of temporal dimension
+            next_states = torch.Tensor(np.array(next_states))[:, 0]
+
+        # convert to torch Tensors
+        actions = torch.LongTensor(actions).unsqueeze(1)  # TODO: enable gpu devices!
+        rewards = torch.Tensor(rewards).unsqueeze(1)
+        imp_weights = torch.Tensor(imp_weights).unsqueeze(1)
+
+        return states, actions, rewards, next_states, trans_indices, imp_weights
+
+
+if __name__ == '__main__':
+    dataset = pd.DataFrame({'episode': np.repeat(np.arange(10), 10),
+                            'timestep': np.tile(np.arange(10), 10),
+                            'state': np.random.random(100),
+                            'action': np.random.random(100),
+                            'reward': np.random.random(100)})
+    replay_buffer = PrioritizedExperienceReplay(dataset, return_history=True)
+
+    for i, w, transition in replay_buffer.sample(N=10):
+        print('Index: ', i)
+        print('Weight:', w)
+        print(transition)
+        print()
---
+++ model/DQN/model.py
@@ -0,0 +1,163 @@
+import copy
+import torch
+import numpy as np
+
+from tqdm import tqdm
+from experience_replay import PrioritizedExperienceReplay
+from performance_tracking import PerformanceTracker
+from loss_functions import weighted_Huber_loss, reward_regularizer
+
+
+class DuelingLayer(torch.nn.Module):
+    """ Implementation of a `Dueling` layer, separating state-
+        value estimation and advantage function as:
+        Q(s, a) = V(s) + [A(s, a) - mean_a(A(s, a))]
+    """
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self._val = torch.nn.Linear(input_dim, 1)
+        self._adv = torch.nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        adv = self._adv(x)
+        val = self._val(x)
+        return val + (adv - torch.mean(adv, dim=1, keepdim=True))
+
+
+class DuelingDQN(torch.nn.Module):
+    """ Implementation of a 'dueling' DQN model (van Hasselt et al., 2015).
+        For details: https://arxiv.org/pdf/1511.06581.pdf
+    """
+    def __init__(self, state_dim=10, num_actions=2, hidden_dims=()):
+        super(DuelingDQN, self).__init__()
+
+        # Shared feature network
+        shape = (state_dim,) + hidden_dims
+        layers = []
+        for i in range(len(shape) - 1):
+            layers.append(torch.nn.Linear(shape[i], shape[i + 1]))
+            layers.append(torch.nn.LeakyReLU())
+        self._base = torch.nn.Sequential(*layers)
+        self._head = DuelingLayer(shape[-1], num_actions)
+
+        # Initialize weights using Xavier initialization
+        self.apply(self._init_xavier_uniform)
+
+        # Move to GPU if available
+        self._device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.to(self._device)
+
+    @staticmethod
+    def _init_xavier_uniform(layer):
+        """ Init weights using Xavier initialization """
+        if isinstance(layer, torch.nn.Linear):
+            torch.nn.init.xavier_uniform_(layer.weight)
+            layer.bias.data.fill_(0.01)
+
+    def forward(self, states):  # -> state.shape = (batch_size, state_dim,)
+        """ Forward pass through DQN """
+        h = self._base(states)
+        return self._head(h)
+
+    def sample(self, states):
+        """ Sample action given state according to policy """
+        states = torch.Tensor(states)
+        with torch.no_grad():
+            actions = torch.argmax(self(states), dim=1)
+        return actions.detach().numpy()
+
+
+def fit_dueling_double_dqn(experiment, policy, dataset, state_cols, action_col, reward_col, episode_col, timestep_col,
+                           num_episodes=1, alpha=1e-3, gamma=0.99, tau=1e-2, lamda=1e-3, eval_func=None, eval_after=100,
+                           batch_size=32, replay_alpha=0.0, replay_beta0=0.4, encoder=None, freeze_encoder=False,
+                           scheduler_gamma=0.9, step_scheduler_after=100, reward_clipping=np.inf):
+
+    replay_buffer = PrioritizedExperienceReplay(dataset, state_cols, action_col, reward_col, episode_col, timestep_col,
+                                                alpha=replay_alpha, beta0=replay_beta0, return_history=bool(encoder))
+
+    optimizer = torch.optim.Adam(policy.parameters(), lr=alpha)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=scheduler_gamma)
+    tracker = PerformanceTracker(experiment)
+
+    target = copy.deepcopy(policy)
+    target.load_state_dict(policy.state_dict())  # Sanity check
+
+    # Freeze encoder's weights (optional)
+    if encoder and freeze_encoder:
+        for param in encoder.parameters():
+            param.requires_grad = False
+        print('Freezing encoder...')
+
+    # Gradient clipping to prevent catastrophic collapse
+    for w in policy.parameters():
+        w.register_hook(lambda grad: torch.clamp(grad, -1, 1))
+
+    #####################
+    #     Training      #
+    #####################
+
+    # Enable training mode
+    policy.train(True)
+    if encoder:
+        encoder.train(True)
+
+    for episode in tqdm(range(num_episodes)):
+
+        states, actions, rewards, next_states, _, weights = replay_buffer.sample(N=batch_size)
+
+        # Encode states if encoder provided
+        if encoder:
+            states = encoder(states)
+            next_states = encoder(next_states)
+
+        # Compute Q-estimate
+        q_pred = policy(states).gather(dim=1, index=actions)
+
+        # Bootstrap Q-target
+        with torch.no_grad():
+            next_model_action = torch.argmax(policy(next_states), dim=1, keepdim=True)
+            q_target = rewards + gamma * target(next_states).gather(dim=1, index=next_model_action)
+
+        # Loss + regularization
+        loss = weighted_Huber_loss(q_pred, q_target, weights)
+        loss += lamda * reward_regularizer(q_pred, reward_clipping)
+
+        # Policy update
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Soft target update
+        with torch.no_grad():
+            for target_w, policy_w in zip(target.parameters(), policy.parameters()):
+                target_w.data = ((1 - tau) * target_w.data + tau * policy_w.data).clone()
+
+        # Update lrate every few episodes
+        if episode % step_scheduler_after == 0 and episode > 0:
+            scheduler.step()
+
+        # TD-error
+        td_error = torch.abs(q_target - q_pred)
+        # TODO: update importance weights using td_error
+
+        ############################
+        #   Performance Tracking   #
+        ############################
+
+        tracker.add('loss', loss.item())
+        tracker.add('avg_Q_value', torch.mean(policy(states)).item())
+        tracker.add('abs_TD_error', torch.mean(td_error))
+
+        if episode % eval_after == 0:
+            if eval_func:
+                avg_reward = eval_func(policy).groupby(episode_col)[reward_col].sum().mean()
+                tracker.add('avg_reward', avg_reward)
+
+            print('\nEp %s/%s: %s' % (episode, num_episodes, tracker.print_stats()))
+
+        tracker.save()
+
+    # Disable training mode
+    policy.train(False)
+    if encoder:
+        encoder.train(False)
