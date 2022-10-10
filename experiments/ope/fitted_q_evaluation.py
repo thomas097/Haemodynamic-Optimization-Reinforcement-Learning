2117e7f98191df875d9aeb094d85e9426b03e4d7
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-10-10 14:30:19 +0200

    Add files via upload

--- experiments/ope/fitted_q_evaluation.py
+++ experiments/ope/fitted_q_evaluation.py
@@ -6,8 +6,8 @@ from sklearn.preprocessing import label_binarize
 
 
 class FQE:
-    def __init__(self, training_file, state_cols, action_col, reward_col, episode_col, method='rf', gamma=0.9,
-                 reg_lambda=0.001, max_iters=250, num_estimators=800, max_depth=5):
+    def __init__(self, training_file, state_cols, action_col, reward_col, episode_col, method='lr', gamma=0.9,
+                 reg_lambda=0.1, max_iters=250, num_estimators=800, max_depth=5):
         """ Implementation of the Fitted Q-Evaluation (FQE) method for OPE.
             Please refer to https://arxiv.org/pdf/2107.11003.pdf for details.
 
@@ -18,7 +18,7 @@ class FQE:
             reward_col:           Column corresponding to reward received
             episode_col:          Column with episode identifiers
 
-            method:               Model used to approximate Q-function ('rf'|'lr', default: 'rf')
+            method:               Model used to approximate Q-function ('rf'|'lr', default: 'lr')
             gamma:                Discount factor
             reg_lambda:           Regularization factor used to suppress unseen actions (when method='lr')
             max_iters:            Maximum number of iterations to run
@@ -27,14 +27,14 @@ class FQE:
         """
         # Create dataset of state-action-tables, next-state-next-action tables and rewards
         df = pd.read_csv(training_file)
-        self._state_action, self._reward = self._create_state_action_table(df, state_cols, action_col, reward_col)
-        self._next_state_action, self._is_next_state = self._create_next_state_action_table(df, state_cols, action_col, episode_col)
+        self._state_action, self._reward, self._episodes = self._create_state_action_table(df, state_cols, action_col, reward_col, episode_col)
+        self._next_state_action, self._is_next_state, self._is_non_terminal = self._create_next_state_action_table(df, state_cols, action_col, episode_col)
 
         # Estimators
         if method == 'rf':
             self._estimator = RandomForestRegressor(num_estimators, max_depth=max_depth, random_state=1)
         elif method == 'lr':
-            self._estimator = Lasso(reg_lambda, random_state=1)
+            self._estimator = Lasso(reg_lambda, max_iter=10000, random_state=1)
         else:
             raise Exception('Method %s not understood' % method)
 
@@ -43,7 +43,7 @@ class FQE:
         self._max_iters = max_iters
 
     @staticmethod
-    def _create_state_action_table(df, state_cols, action_col, reward_col):
+    def _create_state_action_table(df, state_cols, action_col, reward_col, episode_col):
         """ Creates table of shape SxA consisting of states `s` and one-hot
             encoded actions `a`. and a reward vector `r`.
         """
@@ -52,13 +52,14 @@ class FQE:
         states = df[non_terminals][state_cols].values
         actions = df[non_terminals][action_col].values.astype(int)
         rewards = df[non_terminals][reward_col].values
+        episodes = df[non_terminals][episode_col].values
 
         num_actions = int(np.max(df[action_col]) + 1)
 
         # Concatenate states and actions (using one-hot)
         actions_one_hot = label_binarize(actions, classes=range(num_actions))
         state_action_table = np.column_stack([states, actions_one_hot])
-        return state_action_table, rewards
+        return state_action_table, rewards, episodes
 
     @staticmethod
     def _create_next_state_action_table(df, state_cols, action_col, episode_col):
@@ -69,24 +70,26 @@ class FQE:
         is_next_state = df.groupby(episode_col)[episode_col].transform(lambda ep: np.arange(ep.shape[0]) > 0).values
         df = df[is_next_state]
 
-        # Count follow-up states
+        # Identify remaining states that are terminal
+        is_non_terminal = df.groupby(episode_col)[episode_col].transform(lambda ep: np.arange(ep.shape[0]) < ep.shape[0] - 1).values
+
+        # Count mext states
         next_states = df[state_cols].values
         num_states = next_states.shape[0]
 
         # Count available actions
-        num_actions = int(np.max(df[action_col]) + 1)  # -> 25 actions
-        next_actions = np.eye(num_actions)
+        num_actions = int(np.max(df[action_col]) + 1)
+        next_actions = np.eye(num_actions)  # -> all 25 actions!
 
         # Create table with all next-state-next-action combinations
-        next_state_ids = np.arange(num_states)
-        table = np.column_stack([np.repeat(next_states, repeats=num_actions, axis=0),      # Next states
-                                 np.tile(next_actions, reps=(num_states, 1)),              # Next actions
-                                 np.repeat(next_state_ids, repeats=num_actions, axis=0)])  # next state ids (for bookkeeping)
+        table = np.column_stack([np.repeat(next_states, repeats=num_actions, axis=0),             # Next states
+                                 np.tile(next_actions, reps=(num_states, 1)),                     # Next actions
+                                 np.repeat(np.arange(num_states), repeats=num_actions, axis=0)])  # Next state ids
 
         header = state_cols + list(range(num_actions)) + ['next_state_id']
         table = pd.DataFrame(data=table, columns=header)
 
-        return table, is_next_state
+        return table, is_next_state, is_non_terminal
 
     def _fit_estimator(self, pi):
         """ Iteratively estimates V^πe using Fitted Q-Evaluation
@@ -108,14 +111,14 @@ class FQE:
             # Compute πe(a'|s') * Q(a'|s')
             self._next_state_action['V_pi'] = pi_next * self._estimator.predict(next_state_action)
 
-            # Compute expected state value; yi = reward + gamma * sum[πe(a|s) * Q(s, a)]
+            # Compute expected next state value; sum[πe(a|s) * Q(s, a)]
             V_next = self._next_state_action.groupby('next_state_id', sort=False)['V_pi'].sum().values
-            yi = self._reward + self._gamma * V_next
+
+            # Compute expected SARSA target; yi = reward + gamma * V_next
+            yi = self._reward + (self._gamma * V_next * self._is_non_terminal)
 
             # Refit Q-estimator
             self._estimator.fit(self._state_action, yi)
-
-            print(self._estimator.score(self._state_action, yi))
 
     def __call__(self, pi):
         """ Computes the FQE estimate of V^πe.
@@ -126,8 +129,12 @@ class FQE:
         # Fit estimator for Q(s, a)
         self._fit_estimator(pi)
 
-        # Estimate sum_a[πe(a|s) * Q(s, a)] / N
-        return self._next_state_action.groupby('next_state_id', sort=False)['V_pi'].sum().mean()
+        # Get first states of each episode
+        is_first_state = np.where(np.roll(self._episodes, 1) != self._episodes)[0]
+        first_states = self._state_action[is_first_state]
+
+        # Estimate mean discounted cumulative reward
+        return np.mean(self._estimator.predict(first_states))
 
 
 if __name__ == '__main__':
