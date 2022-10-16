651a3c67e34ae129df389012b278e752801a0e60
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-10-16 19:18:44 +0200

    Modified tests

--- ope/importance_sampling.py
+++ ope/importance_sampling.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import label_binarize
+import matplotlib.pyplot as plt
 
 
 class IS:
-    def __init__(self, behavior_policy_file, gamma=0.9):
+    def __init__(self, behavior_policy_file, gamma=1.0):
         """ Implementation of the Stepwise Importance Sampling (IS) estimator.
             Please refer to https://arxiv.org/pdf/1807.01066.pdf for details.
 
@@ -24,7 +24,7 @@ class IS:
         self._timesteps = df.groupby('episode').size().max()
 
         # Reward function (drop terminal time steps without rewards)
-        self._rewards = df['reward'].values.reshape(-1, self._timesteps)[:, :-1]
+        self._rewards = df['reward'].values.reshape(-1, self._timesteps)
         self._gamma = gamma
 
     @staticmethod
@@ -42,24 +42,23 @@ class IS:
         """
         # Extract table of action probabilities acc. to πe (πb) w.r.t. chosen actions
         pi_b = self._pi_b
-        action_probs_e = np.take_along_axis(pi_e, self._actions, axis=1).reshape(-1, self._timesteps)
-        action_probs_b = np.take_along_axis(pi_b, self._actions, axis=1).reshape(-1, self._timesteps)
-
-        # Drop terminal time steps (have NaN rewards)
-        action_probs_e = action_probs_e[:, :-1]
-        action_probs_b = action_probs_b[:, :-1]
+        action_probs_e = np.take_along_axis(pi_e, self._actions, axis=1).reshape(-1, self._timesteps)[:, :-1]
+        action_probs_b = np.take_along_axis(pi_b, self._actions, axis=1).reshape(-1, self._timesteps)[:, :-1]
 
         # Compute cumulative importance ratio
-        w = np.cumprod(action_probs_e / action_probs_b, axis=1)
+        ratio = np.cumprod(action_probs_e / action_probs_b, axis=1)
 
         # Compute gamma as a function of t
         gamma = np.power(self._gamma, np.arange(self._timesteps - 1))[np.newaxis]
 
-        return np.mean(np.sum(gamma * w * self._rewards, axis=1))
+        # Drop NaN reward at terminal states
+        rewards = self._rewards[:, :-1]
+
+        return np.mean(np.sum(gamma * ratio * rewards, axis=1))
 
 
 class WIS(IS):
-    def __init__(self, behavior_policy_file, gamma=0.9):
+    def __init__(self, behavior_policy_file, gamma=1.0):
         """ Implementation of the Stepwise Weighted Importance Sampling (WIS) estimator.
             Please refer to https://arxiv.org/pdf/1807.01066.pdf for details.
 
@@ -79,12 +78,8 @@ class WIS(IS):
         """
         # Extract action probabilities acc. to πe (πb) w.r.t. chosen actions
         pi_b = self._pi_b
-        action_probs_e = np.take_along_axis(pi_e, self._actions, axis=1).reshape(-1, self._timesteps)
-        action_probs_b = np.take_along_axis(pi_b, self._actions, axis=1).reshape(-1, self._timesteps)
-
-        # Drop terminal time steps (no reward received in these states)
-        action_probs_e = action_probs_e[:, :-1]
-        action_probs_b = action_probs_b[:, :-1]
+        action_probs_e = np.take_along_axis(pi_e, self._actions, axis=1).reshape(-1, self._timesteps)[:, :-1]
+        action_probs_b = np.take_along_axis(pi_b, self._actions, axis=1).reshape(-1, self._timesteps)[:, :-1]
 
         # Compute cumulative importance ratio
         ratio = np.cumprod(action_probs_e / action_probs_b, axis=1)
@@ -93,18 +88,21 @@ class WIS(IS):
         # Compute gamma as a function of t
         gamma = np.power(self._gamma, np.arange(self._timesteps - 1))[np.newaxis]
 
-        return np.sum(gamma * w * self._rewards)
+        # Drop NaN reward at terminal states
+        rewards = self._rewards[:, :-1]
+
+        return np.sum(gamma * w * rewards)
 
 
-class GWIS(IS):
-    """ Implementation of the Groupwise Weighted Importance Sampling (GWIS)
+class GroupwiseWIS(IS):
+    """ Implementation of the Groupwise Weighted Importance Sampling (WIS)
         estimator which accounts for confounding factors such as `disease severity`
         known to affect actions AND outcome variables (e.g. `base mortality rate`).
 
-        GWIS groups trajectories by some factor (e.g. `sirs_score`) and computes
-        an average over group-wise WIS estimates (weighted by their support!).
+        GroupwiseWIS groups trajectories by some factor (e.g. `sirs_score`) and computes
+        an average over the group-wise WIS estimates (weighted by their support).
     """
-    def __init__(self, behavior_policy_file, dataset_file, group_by='sirs_score', gamma=0.9):
+    def __init__(self, behavior_policy_file, dataset_file, group_by='sirs_score', gamma=1.0):
         super().__init__(behavior_policy_file, gamma)
         # Assign indices to each group in `group_by`
         groups = pd.read_csv(dataset_file, usecols=[group_by])[group_by].values
@@ -158,39 +156,54 @@ class GWIS(IS):
 
 if __name__ == '__main__':
     # Behavior policy
-    behavior_df = pd.read_csv('physician_policy/mimic-iii_train_behavior_policy.csv')
-    behavior_policy = behavior_df[[str(i) for i in range(25)]].values
+    behavior_df = pd.read_csv('physician_policy/roggeveen_4h/mimic-iii_valid_behavior_policy.csv')
+    behavior_policy = behavior_df[[str(i) for i in range(25)]].values  # assume 25 actions
 
     # Random policy
     random_policy = np.random.uniform(0.0, 1, behavior_policy.shape)
     random_policy = random_policy / np.sum(random_policy, axis=1, keepdims=True)
 
+    # Perfect policy
+    def get_perfect_action(x):
+        action1 = x * 0
+        action1['1'] = 1
+        return x if x['reward'].values[-2] == 15 else action1  # just behavior actions if R=15, else action 1
+    perfect_policy = behavior_df.groupby('episode').apply(get_perfect_action)[[str(i) for i in range(25)]].values
+
     # Zero policy (inaction)
     zero_policy = np.zeros(behavior_policy.shape)
     zero_policy[:, 0] = 1
 
-    is_ = IS('physician_policy/mimic-iii_train_behavior_policy.csv')
-    wis = WIS('physician_policy/mimic-iii_train_behavior_policy.csv')
-    gwis = GWIS('physician_policy/mimic-iii_train_behavior_policy.csv',
-                '../preprocessing/datasets/mimic-iii/roggeveen/mimic-iii_train.csv')
+    is_ = IS('physician_policy/roggeveen_4h/mimic-iii_valid_behavior_policy.csv')
+    wis = WIS('physician_policy/roggeveen_4h/mimic-iii_valid_behavior_policy.csv')
+    gwis = GroupwiseWIS('physician_policy/roggeveen_4h/mimic-iii_valid_behavior_policy.csv',
+                        '../preprocessing/datasets/mimic-iii/roggeveen_4h/mimic-iii_valid.csv')
 
     print('IS:')
-    print('behavior:', is_(behavior_policy))
-    print('random:  ', is_(random_policy))
-    print('zero:    ', is_(zero_policy))
+    print('behavior: ', is_(behavior_policy))
+    print('perfect:  ', is_(perfect_policy))
+    print('random:   ', is_(random_policy))
+    print('zero:     ', is_(zero_policy))
 
     print('\nWIS:')
-    print('behavior:', wis(behavior_policy))
-    print('random:  ', wis(random_policy))
-    print('zero:    ', wis(zero_policy))
+    print('behavior: ', wis(behavior_policy))
+    print('perfect:  ', wis(perfect_policy))
+    print('random:   ', wis(random_policy))
+    print('zero:     ', wis(zero_policy))
 
-    print('\nGWIS:')
-    print('behavior:', gwis(behavior_policy, return_groups=True))
-    print('random:  ', gwis(random_policy, return_groups=True))
-    print('zero:    ', gwis(zero_policy, return_groups=True))
+    # print('\nGWIS:')
+    # print('behavior: ', gwis(behavior_policy), gwis(behavior_policy, return_groups=True))
+    # print('random:   ', gwis(random_policy), gwis(random_policy, return_groups=True))
+    # print('foresight:', gwis(zero_policy), gwis(random_policy, return_groups=True))
+    # print('zero:     ', gwis(zero_policy), gwis(zero_policy, return_groups=True))
 
-    # Sanity check: estimate true discounted reward
+    # Sanity check: estimate true reward
     rewards = behavior_df['reward'].values.reshape(-1, 18)[:, :-1]
-    gamma = np.power(0.9, np.arange(17))[np.newaxis]
-    discounted_reward = np.mean(np.sum(gamma * rewards, axis=1))
-    print('\nTrue discounted reward of pi_b:', discounted_reward)
+    total_reward = np.mean(np.sum(rewards, axis=1))
+    print('\nTrue total reward of pi_b:', total_reward)
+
+    # Sanity check: estimate total reward of zero-drug trajectories
+    actions = behavior_df['action'].values.reshape(-1, 18)[:, :-1]
+    rewards = rewards[actions.max(axis=1) == 0]
+    total_reward = np.mean(np.sum(rewards, axis=1))
+    print('Expected zero-drug reward:', total_reward)
