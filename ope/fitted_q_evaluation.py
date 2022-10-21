9e82ae72dd433f83a5774b1b2e6531e888620b3a
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-10-21 13:41:29 +0200

    Add files via upload

--- ope/fitted_q_evaluation.py
+++ ope/fitted_q_evaluation.py
@@ -1,153 +1,172 @@
+import torch
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import label_binarize
-from estimators import *
+from tqdm import tqdm
+
+
+class FuncApproximator(torch.nn.Module):
+    def __init__(self, state_dims, num_actions):
+        super().__init__()
+        self._estimator = torch.nn.Sequential(
+            torch.nn.Linear(state_dims, num_actions)
+        )
+
+    def forward(self, states, hard_actions=None, action_probs=None):
+        q_pred = self._estimator(states)
+        if hard_actions is not None:
+            return q_pred.gather(dim=1, index=hard_actions.unsqueeze(1))[:, 0]
+        if action_probs is not None:
+            return torch.sum(action_probs * q_pred, axis=1)
+        return q_pred
 
 
 class FittedQEvaluation:
-    """ Implementation of the Fitted Q-Evaluation (FQE) estimator for Off-policy
-        Policy Evaluation (OPE). For details, see https://arxiv.org/pdf/2102.03607.pdf
+    """ Implementation of the Bootstrapped Fitted Q-Evaluation (FQE) estimator for
+        Off-policy Policy Evaluation (OPE). For details, see:
+        http://proceedings.mlr.press/v139/hao21b/hao21b.pdf
     """
-    def __init__(self, states, actions, rewards, episodes, method='rf', gamma=0.9, max_iters=25, verbose=False):
-        # Terminal state + start state masks
-        self._is_start_state = np.insert(episodes[1:] != episodes[:-1], 0, True)  # Checked
-        self._is_terminal_state = np.append(episodes[1:] != episodes[:-1], True)  # Checked
-        self._action_space = sorted(set(actions[~np.isnan(actions)]))             # Checked
-
-        # Create dataset of ((state, action), reward) pairs
-        self._states_actions = self._create_state_action_table(states, actions, non_terminal_only=True)
-        self._rewards = rewards[~self._is_terminal_state]
-
-        # Q-table with every possible state-action pair
-        self._Q_table = self._create_Q_table(states, self._action_space, next_states_only=True)
-
-        # Estimation method
-        if method == 'lasso':
-            self._estimator = LassoRegression()
-        elif method == 'rf':
-            self._estimator = RandomForest()
-        else:
-            raise Exception('Method %s not understood' % method)
-
-        # Hyper-parameters
+    def __init__(self, state_dims, num_actions, num_timesteps, gamma=0.9, lrate=1e-2, initial_iters=1000, step_iters=500, reg=1e-2):
+        self._num_timesteps = num_timesteps
         self._gamma = gamma
-        self._max_iters = max_iters
-        self._verbose = verbose
-        self.is_fitted = False
+        self._lrate = lrate
 
-    def _create_state_action_table(self, states, actions, non_terminal_only=True):
-        # Drop terminal states (no action, nor reward)
-        if non_terminal_only:
-            states = states[~self._is_terminal_state]
-            actions = actions[~self._is_terminal_state]
+        # Model + warm start parameters
+        self._estimator = FuncApproximator(state_dims + 1, num_actions)
+        self._initial_iters = initial_iters
+        self._step_iters = step_iters
 
-        # Concatenate states and dummy-encoded actions
-        num_actions = np.max(self._action_space) + 1
-        action_dummies = label_binarize(actions, classes=np.arange(num_actions))
-        states_actions = np.column_stack([states, action_dummies])
+        self._criterion = torch.nn.MSELoss()
+        self._optimizer = torch.optim.SGD(self._estimator.parameters(), lr=lrate, weight_decay=reg)
+        self.__fitted = False
 
-        return states_actions
+    def fit(self, states, actions, rewards, next_states, policy_next_action_probs):
+        """
+        Fits FQE estimator for Q^pi to states, actions, rewards
+        and next states obtained through a behavior policy.
+        """
+        # To Pytorch Tensors
+        states = torch.Tensor(states)
+        actions = torch.LongTensor(actions)
+        rewards = torch.Tensor(rewards)
+        next_states = torch.Tensor(next_states)
+        policy_next_action_probs = torch.Tensor(policy_next_action_probs)
 
-    def _create_Q_table(self, states, action_space, next_states_only=True):
-        # Drop 'first states' as they are not 'next states'
-        if next_states_only:
-            states = states[~self._is_start_state]
+        # Add timestep feature to states and next_states
+        num_episodes = states.shape[0] // (self._num_timesteps - 1)
+        timesteps = torch.arange(self._num_timesteps - 1).repeat(num_episodes).unsqueeze(1)
+        states = torch.concat([states, timesteps], dim=1)
+        next_states = torch.concat([next_states, timesteps + 1], dim=1)
 
-        # Create table of possible actions
-        num_actions = np.max(action_space) + 1
-        action_dummies = np.eye(num_actions)
+        # Warm start -- continue training from last estimator
+        iters = self._step_iters if self.__fitted else self._initial_iters
 
-        # Create Q table of |S|x|A| rows with all next-state-next-action combinations
-        num_states, state_dims = states.shape
-        table = np.column_stack([np.repeat(states, repeats=num_actions, axis=0),          # Next states
-                                 np.tile(action_dummies, reps=(num_states, 1)),           # Next actions
-                                 np.zeros((num_states * num_actions, 1)),                 # Q-value (initially zero)
-                                 np.repeat(np.arange(num_states), repeats=num_actions)])  # Next state ID
+        # Mask out expected future reward at next state if terminal
+        reward_mask = (rewards == 0).float()
 
-        # Create DataFrame for efficiency
-        header = ['x%s' % i for i in range(state_dims + num_actions)] + ['policy_q', 'next_state_id']
-        return pd.DataFrame(data=table, columns=header)
+        # Perform policy iteration
+        pbar = tqdm(range(iters))
+        for _ in pbar:
 
-    def fit(self, policy):
-        # Initial fit of estimator setting Q(s, a) = 0
-        self._estimator.fit(self._states_actions, np.zeros(self._rewards.shape))
-        prev_q = 0.0
+            # Bootstrapped target using policy πe!
+            with torch.no_grad():
+                q_next = rewards + self._gamma * reward_mask * self._estimator(next_states, action_probs=policy_next_action_probs)
 
-        # Limit pi(a|s) to 'next' actions and 'next' states
-        policy = policy[~self._is_start_state].flatten()
+            # Current Q-estimate
+            q_pred = self._estimator(states, hard_actions=actions)
 
-        # Separate next-state-next-action part from Q-table
-        next_states_actions = self._Q_table.values[:, :-2]
+            self._optimizer.zero_grad()
+            self._criterion(q_pred, q_next).backward()
+            self._optimizer.step()
 
-        # Mask states in self._states_actions that are pre-terminal states,
-        # i.e. states that transition to terminal states (must not receive V(s'))
-        to_terminal = np.roll(self._is_terminal_state, shift=-1)[~self._is_terminal_state]  # Checked
+            # Show Q- and V-estimates during training
+            start_q_pred = q_pred[0::self._num_timesteps - 1]  # Compute only over start-states!
+            pbar.set_postfix({'avg_q': torch.mean(q_pred).item(),
+                              'max_q': torch.max(q_pred).item(),
+                              'avg_v_start': torch.mean(start_q_pred).item()})
 
-        for it in range(self._max_iters):
-            # Predict Q(s', a') using estimator for Q
-            q_values = self._estimator.predict(next_states_actions)
+        # Indicate that estimator has some fit
+        self.__fitted = True
+        return self
 
-            # Compute expected SARSA 'next state'-value: sum_a[pi(a|s) * Q(s', a')]
-            self._Q_table['policy_q'] = policy * q_values
-            next_state_value = self._Q_table.groupby('next_state_id', sort=False)['policy_q'].sum()
+    def value(self, start_states, policy_action_probs):
+        """
+        Returns the estimated state value, V(s), according to the
+        greedy policy on which the FQE instance was fitted.
+        """
+        if not self.__fitted:
+            raise Exception('Estimator has not been fit; Call fit().')
+        # Add timestep column to input (0 as we are concerned with start states)
+        start_states = np.column_stack([start_states, np.zeros(start_states.shape[0])])
 
-            # Bootstrap Q-estimate
-            y = self._rewards + self._gamma * (next_state_value * to_terminal)
-
-            # Refit estimator
-            self._estimator.fit(self._states_actions, y)
-
-            if self._verbose:
-                avg_q = np.mean(q_values)
-                avg_loss = np.mean(np.absolute(prev_q - y))
-                print('It %s/%s: Mean Q-value = %.3f  MAE = %.3f' % (it + 1, self._max_iters, avg_q, avg_loss))
-            prev_q = y
-
-    def Q(self, states, actions):
-        # Create state-action table with dummy-encoded actions
-        states_actions = self._create_state_action_table(states, actions, non_terminal_only=False)
-        return self._estimator.predict(states_actions)
-
-    def V(self, states, policy):
-        # Create Q-table with all possible state-action pairs
-        Q_table = self._create_Q_table(states, self._action_space, next_states_only=False)
-
-        # Predict Q(s', a') using estimator (limit to state-action part of Q-table)
-        states_actions = Q_table.values[:, :-2]
-        q_values = self._estimator.predict(states_actions)
-
-        # Predict V(s) = sum_a[pi(a|s) * Q(s, a)]
-        Q_table['policy_q'] = policy.flatten() * q_values
-        return Q_table.groupby('next_state_id', sort=False)['policy_q'].sum().values
+        # Average state value over start states
+        start_states = torch.Tensor(start_states)
+        action_probs = torch.Tensor(policy_action_probs)
+        state_values = self._estimator(start_states, action_probs=action_probs)
+        return torch.mean(state_values).detach().numpy()
 
 
 if __name__ == '__main__':
-    training_data = pd.read_csv('../../preprocessing/datasets/mimic-iii/roggeveen/mimic-iii_train.csv')
+    train_df = pd.read_csv('../preprocessing/datasets/mimic-iii/roggeveen_4h/mimic-iii_train.csv')
 
     # Unpack training dataset into states, actions, rewards and episode IDs
     meta_data = ['icustay_id', 'timestep', 'max_vp_shifted', 'total_iv_fluid_shifted', 'reward', 'action', 'state_sirs']
-    actions = training_data['action'].values.astype(np.uint8)
-    rewards = training_data['reward'].values
-    episodes = training_data['icustay_id'].values.astype(np.uint64)
-    states = training_data[[c for c in training_data.columns if c not in meta_data]].values
+    all_states = train_df[[c for c in train_df.columns if c not in meta_data]].values.astype(np.float32)
+    all_actions = train_df['action'].values.astype(np.uint8)
+    all_rewards = train_df['reward'].values.astype(np.float32)
+    episodes = train_df['icustay_id'].values.astype(np.uint32)
 
-    # Identify start states s0 and action space
-    is_start_state = np.insert(episodes[1:] != episodes[:-1], 0, True)
+    # Define (non-terminal) states as not 'last states' in trajectory
+    last_state_ids = len(episodes) - np.unique(episodes[::-1], return_index=1)[1] - 1
+    states = np.delete(all_states, last_state_ids, axis=0)
+    actions = np.delete(all_actions, last_state_ids, axis=0)
+    rewards = np.delete(all_rewards, last_state_ids, axis=0)
 
-    # Unpack behavior policy
-    behavior_df = pd.read_csv('physician_policy/mimic-iii_train_behavior_policy.csv')
-    behavior_policy = behavior_df[[str(i) for i in range(25)]].values
+    # Define 'next states' as not 'first states'
+    first_state_ids = np.unique(episodes, return_index=1)[1]
+    next_states = np.delete(all_states, first_state_ids, axis=0)
 
-    # Fit FQE
-    FQE = FittedQEvaluation(states, actions, rewards, episodes, method='rf', verbose=True)
-    FQE.fit(behavior_policy)
+    #####################
+    #     Policies
+    #####################
 
-    # Predict V(s) an Q(s, a)
-    print('Mean Q(s0, a0):', np.mean(FQE.Q(states[is_start_state], actions[is_start_state])))
-    print('Mean V(s0):    ', np.mean(FQE.V(states[is_start_state], behavior_policy[is_start_state])))
+    # Behavior policy
+    behavior_df = pd.read_csv('physician_policy/roggeveen_4h/mimic-iii_train_behavior_policy.csv')
+    behavior_action_probs = behavior_df[[str(i) for i in range(25)]].values  # assume 25 actions
+    behavior_next_action_probs = np.delete(behavior_action_probs, first_state_ids, axis=0)
 
-    # Sanity check: Expected discounted reward?
-    rewards = rewards.reshape(-1, 18)[:, :-1]
-    gamma = np.power(0.9, np.arange(17))[np.newaxis]
-    discounted_reward = np.mean(np.sum(gamma * rewards, axis=1))
-    print('\nTrue reward of pi_b:', discounted_reward)
+    # Zero policy
+    zerodrug_action_probs = np.zeros(behavior_action_probs.shape)
+    zerodrug_action_probs[:, 0] = 1
+    zerodrug_next_action_probs = np.delete(zerodrug_action_probs, first_state_ids, axis=0)
+
+    # Random policy
+    np.random.seed(42)
+    random_action_probs = np.random.uniform(0, 1, behavior_action_probs.shape)
+    random_action_probs = random_action_probs / np.sum(random_action_probs, axis=1, keepdims=True)
+    random_next_action_probs = np.delete(random_action_probs, first_state_ids, axis=0)
+
+    #####################
+    #     Estimation
+    #####################
+
+    # Fit FQEs
+    print('V(s_0):')
+    fqe = lambda: FittedQEvaluation(state_dims=states.shape[1], num_actions=25, num_timesteps=18)
+
+    estimator = fqe().fit(states, actions, rewards, next_states, policy_next_action_probs=behavior_next_action_probs)
+    print('Behavior: ', estimator.value(all_states[first_state_ids], behavior_action_probs[first_state_ids]))
+
+    estimator = fqe().fit(states, actions, rewards, next_states, policy_next_action_probs=zerodrug_next_action_probs)
+    print('Zero-drug:', estimator.value(all_states[first_state_ids], zerodrug_action_probs[first_state_ids]))
+
+    estimator = fqe().fit(states, actions, rewards, next_states, policy_next_action_probs=random_next_action_probs)
+    print('Random:   ', estimator.value(all_states[first_state_ids], random_action_probs[first_state_ids]))
+
+    # Sanity check (note: FQE is biased but relative estimates should make sense)
+    rewards = all_rewards.reshape(-1, 18)[:, :-1]
+    gamma_t = np.power(0.9, np.arange(17))[np.newaxis]
+    total_reward = np.mean(np.sum(gamma_t * rewards, axis=1))
+    print('\nEmpirical discounted reward of behavior policy:', total_reward)
+
+
+
