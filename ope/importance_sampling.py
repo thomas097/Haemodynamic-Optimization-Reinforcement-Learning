a8d2053b599974a1a02d9b54677d7f699d602ddf
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-10-29 11:33:10 +0200

    nonlinear WIS to fix degeneracy (WIP)

--- ope/importance_sampling.py
+++ ope/importance_sampling.py
@@ -1,22 +1,22 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import label_binarize
 
 
 class IS:
-    def __init__(self, behavior_policy_file, gamma=1.0, drop_terminal_states=True):
+    def __init__(self, behavior_policy_file, gamma=1.0, clip_degenerate=0):
         """ Implementation of the Stepwise Importance Sampling (IS) estimator.
             Please refer to https://arxiv.org/pdf/1807.01066.pdf for details.
 
             Params
             behavior_policy_file: Path to DataFrame containing action probabilities (columns '0'-'24') for
                                   behavior policy, chosen actions ('action') and associated rewards ('reward').
-            gamma:                Discount factor
-            drop_terminal_states: Whether to discard absorbing terminal states without rewards (default: True)
+            gamma:                Discount factor (default: 1.0)
+            clip_degenerate:      For sharp behavior policies (e.g. softmax), weight degeneracy might result in
+                                  unreliable estimates. Setting `clip_degenerate` will clip the top-N weights
+                                  to their empirical mean (ClipIS), thereby reducing degeneracy.
+                                  For details, see (Martino et al., 2018): https://ieeexplore.ieee.org/document/8450722
         """
         phys_df = pd.read_csv(behavior_policy_file)
-        if drop_terminal_states:
-            phys_df = phys_df[phys_df['reward'].notna()]
 
         # Actions taken by behavior policy (i.e. physician)
         self.actions = np.expand_dims(phys_df['action'], axis=1).astype(int)
@@ -28,6 +28,8 @@ class IS:
         self.timesteps = phys_df.groupby('episode').size().max()
         self.rewards = self._to_table(phys_df['reward'].values)
         self.gamma = np.power(gamma, np.arange(self.timesteps))[np.newaxis]
+        self._clip_degenerate = clip_degenerate
+
 
     @staticmethod
     def _behavior_policy(df):
@@ -35,7 +37,19 @@ class IS:
         return df.filter(regex='\d+').values.astype(np.float64)
 
     def _to_table(self, arr):
-        return np.nan_to_num(arr.reshape(-1, self.timesteps), nan=0.0)  # Replace NaNs in reward table by zeros
+        return arr.reshape(-1, self.timesteps)
+
+    def _clip(self, weights):       
+        # Determine threshold to mask top-k weights
+        sorted_weights = np.sort(weights, axis=0)
+        thres = sorted_weights[-self._clip_degenerate]
+        print(thres)
+
+        # Set weights > thres equal to their empirical mean
+        mask = weights >= thres
+        weights[mask] = np.mean(weights[mask])
+
+        return weights
 
     def __call__(self, pi_e, return_weights=False):
         """ Computes the IS estimate of V^πe.
@@ -51,24 +65,30 @@ class IS:
 
         # Compute cumulative importance ratio
         weights = np.cumprod(action_probs_e / action_probs_b, axis=1)
+        if self._clip_degenerate > 0:
+            weights = self._clip(weights)
+
         if return_weights:
-            return ratio
+            return weights
 
         return np.mean(np.sum(self.gamma * weights * self.rewards, axis=1))
 
 
 class WeightedIS(IS):
-    def __init__(self, behavior_policy_file, gamma=1.0, drop_terminal_states=True):
+    def __init__(self, behavior_policy_file, gamma=1.0, clip_degenerate=0):
         """ Implementation of the Stepwise Weighted Importance Sampling (WIS) estimator.
             Please refer to https://arxiv.org/pdf/1807.01066.pdf for details.
 
             Params
             behavior_policy_file: Path to DataFrame containing action probabilities (columns '0'-'24') for
                                   behavior policy, chosen actions ('action') and associated rewards ('reward').
-            gamma:                Discount factor
-            drop_terminal_states: Whether to discard absorbing terminal states without rewards (default: True)
+            gamma:                Discount factor (default: 1.0)
+            clip_degenerate:      For sharp behavior policies (e.g. softmax), weight degeneracy might result in
+                                  unreliable estimates. Setting `clip_degenerate` will clip the top-N weights
+                                  to their empirical mean (ClipIS), thereby reducing degeneracy.
+                                  For details, see (Martino et al., 2018): https://ieeexplore.ieee.org/document/8450722
         """
-        super().__init__(behavior_policy_file, gamma, drop_terminal_states)
+        super().__init__(behavior_policy_file, gamma, clip_degenerate)
 
     def __call__(self, pi_e, return_weights=False):
         """ Computes the WIS estimate of V^πe.
@@ -84,7 +104,11 @@ class WeightedIS(IS):
 
         # Compute cumulative importance ratio
         ratio = np.cumprod(action_probs_e / action_probs_b, axis=1)
-        weights = ratio / (ratio.sum(axis=0, keepdims=True) + 1e-12)
+        if self._clip_degenerate > 0:
+            ratio = self._clip(ratio)
+
+        # Normalize weights
+        weights = ratio / ratio.sum(axis=0, keepdims=True)
         if return_weights:
             return weights
 
@@ -92,43 +116,62 @@ class WeightedIS(IS):
 
 
 if __name__ == '__main__':
+    np.set_printoptions(suppress=True)
+
     # Behavior policy
-    behavior_df = pd.read_csv('physician_policy/roggeveen_4h/mimic-iii_valid_behavior_policy.csv')
-    behavior_df = behavior_df[behavior_df['reward'].notna()]
+    behavior_policy_file = 'physician_policy/roggeveen_4h_with_cv/mimic-iii_valid_behavior_policy.csv'
+    behavior_df = pd.read_csv(behavior_policy_file)
     behavior_policy = behavior_df.filter(regex='\d+').values  # -> 25 actions marked by integers 0 to 24!
 
     # Random policy
+    from sklearn.preprocessing import label_binarize
     random_policy = label_binarize(np.random.randint(0, 24, behavior_policy.shape[0]), classes=np.arange(25))
-    random_policy = random_policy.astype(np.float32) + np.random.uniform(0, 0.05, random_policy.shape) # Add noise
+    random_policy = random_policy.astype(np.float32) + np.random.uniform(0, 0.05, random_policy.shape)  # Add noise
     random_policy = random_policy / np.sum(random_policy, axis=1, keepdims=True)
-
-    # Bad policy (high likelihood of action 1 which is not possible)
-    bad_policy = np.ones(behavior_policy.shape)
-    bad_policy[:, 1] = 50
-    bad_policy = bad_policy / np.sum(bad_policy, axis=1, keepdims=True)
 
     # Zero-drug policy
     zerodrug_policy = np.full(behavior_policy.shape, fill_value=1e-12)
     zerodrug_policy[:, 0] = 1
 
-    imp_sampling = IS('physician_policy/roggeveen_4h/mimic-iii_valid_behavior_policy.csv')
-    wimp_sampling = WeightedIS('physician_policy/roggeveen_4h/mimic-iii_valid_behavior_policy.csv')
+    # IS/WIS + clipping
+    imp_sampling = IS(behavior_policy_file)
+    imp_clipped = IS(behavior_policy_file, clip_degenerate=80)
+    
+    wimp_sampling = WeightedIS(behavior_policy_file)
+    wimp_clipped = WeightedIS(behavior_policy_file, clip_degenerate=80)
 
     print('IS:')
     print('behavior: ', imp_sampling(behavior_policy))
     print('random:   ', imp_sampling(random_policy))
-    print('bad:      ', imp_sampling(bad_policy))
     print('zero drug:', imp_sampling(zerodrug_policy))
+
+    print('\nIS + clipping:')
+    print('behavior: ', imp_clipped(behavior_policy))
+    print('random:   ', imp_clipped(random_policy))
+    print('zero drug:', imp_clipped(zerodrug_policy))
+
+    print('\n', '-' * 32)
 
     print('\nWIS:')
     print('behavior: ', wimp_sampling(behavior_policy))
     print('random:   ', wimp_sampling(random_policy))
-    print('bad:      ', wimp_sampling(bad_policy))
     print('zero drug:', wimp_sampling(zerodrug_policy))
 
+    print('\nWIS + clipping:')
+    print('behavior: ', wimp_clipped(behavior_policy))
+    print('random:   ', wimp_clipped(random_policy))
+    print('zero drug:', wimp_clipped(zerodrug_policy))
+
+    print('\n', '-' * 32)
+
     # Sanity check: estimate true reward
-    rewards = np.nan_to_num(behavior_df['reward'].values.reshape(-1, 18), nan=0.0)
+    rewards = behavior_df['reward'].values.reshape(-1, 18)
     print('\nSupport:', rewards.shape[0])
 
     total_reward = np.mean(np.sum(rewards, axis=1))
     print('True total reward of pi_b:', total_reward)
+
+    # Plot distribution of weights
+    import matplotlib.pyplot as plt
+    
+    
