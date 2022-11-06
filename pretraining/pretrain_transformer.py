f9b924de45f745e5309be85359d77cb6e1d828ad
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-11-06 22:10:37 +0100

    Add files via upload

--- pretraining/pretrain_transformer.py
+++ pretraining/pretrain_transformer.py
@@ -1,29 +1,35 @@
+import torch
 import numpy as np
 import pandas as pd
 from transformer_models import CausalTransformer
 from pretraining import fit_behavior_cloning
-from utils import count_parameters
+from utils import count_parameters, load_data
 
 
 if __name__ == '__main__':
     # Training and validation dataset
-    train_df = pd.read_csv('../../preprocessing/datasets/mimic-iii/roggeveen_4h/mimic-iii_train.csv')
-    valid_df = pd.read_csv('../../preprocessing/datasets/mimic-iii/roggeveen_4h/mimic-iii_valid.csv')
-    print('train_df.size = %d  valid_df.size = %d' % (len(train_df), len(valid_df)))
+    train_df = load_data('../preprocessing/datasets/mimic-iii/attention_4h/mimic-iii_train.csv')
+    valid_df = load_data('../preprocessing/datasets/mimic-iii/attention_4h/mimic-iii_valid.csv')
 
-    # Set up model
-    transformer = CausalTransformer(in_channels=48, out_channels=96, d_model=64, d_key=32, n_blocks=3)
+    # Set up encoder and classifier head
+    transformer = CausalTransformer(vocab_size=46, out_channels=96, d_model=64, n_heads=4, n_blocks=2, d_key=16)
     print('Transformer parameters:', count_parameters(transformer))
 
-    fit_behavior_cloning(experiment_name='results/transformer_pretraining',
+    classifier = torch.nn.Sequential(
+        torch.nn.Linear(96, 96),
+        torch.nn.LeakyReLU(),
+        torch.nn.Linear(96, 25)
+    )
+
+    # Train!
+    fit_behavior_cloning(experiment='results/transformer_v2_pretraining',
                          encoder=transformer,
-                         classif_layer_shapes=(96, 64),
-                         num_actions=25,
-                         train_dataset=train_df,
-                         valid_dataset=valid_df,
-                         oversample_vaso=5,  # To account for small number of vaso treatments in training set
-                         lrate=1e-4,
+                         classifier=classifier,
+                         train_data=train_df,
+                         valid_data=valid_df,
+                         lrate=1e-3,
                          epochs=100,
-                         batch_size=32,
-                         eval_after=1,
-                         save_on_best=True)
+                         batches_per_epoch=1000,
+                         truncate=256,
+                         batch_size=8,
+                         save_on='valid_loss')
