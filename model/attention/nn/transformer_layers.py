613ddc940cee63aca01117fb536dd8cd3997b8bf
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-11-06 22:09:40 +0100

    Add files via upload

--- model/attention/nn/transformer_layers.py
+++ model/attention/nn/transformer_layers.py
@@ -1,66 +1,136 @@
 import torch
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
 
 
 class TransformerEncoderLayer(torch.nn.Module):
-    """
-    Simplified Implementation of PyTorch's TransformerEncoderLayer
-    with residual connections and layer normalization
-    """
-    def __init__(self, d_model, layer_norm_eps=1e-5, d_key=32, dropout=0.1):
+    def __init__(self, d_model, n_heads, d_key, dropout=0.1):
+        """
+        Transformer encoder block with residual connections and layer normalization
+
+        :param d_model:   Latent dimensions of the encoder
+        :param n_heads:   Number of self-attention heads
+        :param d_key:     Dimensions of queries/keys in self-attention computation
+        :param dropout:   Dropout rate (default: 0.1)
+        """
         super(TransformerEncoderLayer, self).__init__()
-        self.self_attn = SelfAttention(d_model, d_key=d_key)
+        assert n_heads > 0 and d_model > 0 and isinstance(n_heads, int)
+
+        # If one head is used, we use simple SA instead
+        if n_heads < 2:
+            self.self_attn = SelfAttention(d_model, d_key=d_key)
+        else:
+            self.self_attn = MultiHeadSelfAttention(d_model, n_heads=n_heads, d_key=d_key)
+
         self.feedforward = torch.nn.Linear(d_model, d_model)
-        self.layer_norm1 = torch.nn.LayerNorm(d_model, eps=layer_norm_eps)
-        self.layer_norm2 = torch.nn.LayerNorm(d_model, eps=layer_norm_eps)
+        self.layer_norm1 = torch.nn.LayerNorm(d_model)
+        self.layer_norm2 = torch.nn.LayerNorm(d_model)
         self.dropout1 = torch.nn.Dropout(p=dropout)
         self.dropout2 = torch.nn.Dropout(p=dropout)
         self.leaky_relu = torch.nn.LeakyReLU()
 
-    def forward(self, x, attn_mask):
-        z = self.layer_norm1(x + self.self_attn(x, attn_mask=attn_mask))
-        z = self.dropout1(z)
-        y = self.layer_norm2(z + self.leaky_relu(self.feedforward(z)))
-        return self.dropout2(y)
+    def forward(self, x, src_mask, distances):
+        """
+        Forward pass through encoder block
+
+        :param x:           Input tensor of shape (batch_size, num_timesteps, d_model)
+        :param src_mask:    Attention mask of shape (batch_size, num_timesteps, num_timesteps)
+        :param distances:   Distance tensor of shape (batch_size, num_timesteps, num_timesteps)
+        :return:            Tensor of shape (batch_size, num_timesteps, d_model)
+        """
+        # Self-attention + residual connections
+        x_attn = self.self_attn(x, src_mask=src_mask, distances=distances)
+        z = self.dropout1(self.layer_norm1(x + x_attn))
+
+        # Feed forward + residual connections
+        z_ff = self.leaky_relu(self.feedforward(z))
+        return self.dropout2(self.layer_norm2(z + z_ff))
+
+
+class MultiHeadSelfAttention(torch.nn.Module):
+    def __init__(self, d_model, n_heads, d_key):
+        """
+        Implementation of Multi-Head Self-Attention (Vaswani et al., 2017)
+
+        :param d_model:  Latent dimensions of the encoder
+        :param n_heads:  Number of self-attention heads
+        :param d_key:    Dimensions of queries/keys in self-attention computation
+        """
+        super(MultiHeadSelfAttention, self).__init__()
+        assert n_heads > 0 and d_model % n_heads == 0
+
+        self._heads = torch.nn.ModuleList()
+        for _ in range(n_heads):
+            self._heads.append(SelfAttention(d_model, d_key=d_key, d_val=d_model // n_heads))
+
+        self._feedforward = torch.nn.Linear(d_model, d_model)
+
+    def forward(self, x, src_mask, distances):
+        """
+        Forward pass through Self-Attention layer
+
+        :param x:           Input tensor of shape (batch_size, num_timesteps, d_model)
+        :param src_mask:    Attention mask of shape (batch_size, num_timesteps, num_timesteps)
+        :param distances:   Distance tensor of shape (batch_size, num_timesteps, num_timesteps)
+        :return:            Tensor of shape (batch_size, num_timesteps, d_model)
+        """
+        y = torch.concat([h(x, src_mask, distances) for h in self._heads], dim=2)
+        return self._feedforward(y)
 
 
 class SelfAttention(torch.nn.Module):
-    """
-    Implementation of a Simple Self-Attention layer
-    """
-    def __init__(self, d_model, d_key=32):
+    def __init__(self, d_model, d_key=32, d_val=None):
+        """
+        Self-Attention Module (Vaswani et al., 2017)
+
+        :param d_model:  Latent dimensions of the encoder
+        :param d_key:    Dimensions of queries/keys in self-attention computation
+        :param d_val:    Dimensions of values in self-attention computation (optional).
+                         If `d_val=None`, it is assumed d_val == d_model.
+        """
         super(SelfAttention, self).__init__()
+        # Allow output dimensions different from d_model
+        d_val = d_val if d_val is not None else d_model
+
         self._Q = torch.nn.Linear(d_model, d_key, bias=False)
         self._K = torch.nn.Linear(d_model, d_key, bias=False)
-        self._V = torch.nn.Linear(d_model, d_model, bias=False)
-
-        dk = torch.sqrt(torch.tensor(d_key))
-        self._dk = torch.nn.Parameter(dk, requires_grad=False)
+        self._V = torch.nn.Linear(d_model, d_val, bias=False)
+        self._p0 = torch.nn.Parameter(torch.tensor(1.0))
+        self._p1 = torch.nn.Parameter(torch.tensor(1.0))
 
     @staticmethod
-    def _softmax(x, dim):
-        """ Computes softmax while taking care of -inf entries """
+    def _softmax(x):
+        """
+        Numerically stable implementation of softmax
+
+        :param x:  Attention matrix of shape (batch_size, num_timesteps, num_timesteps)
+        :return:   Normalized attention matrix
+        """
         z = torch.exp(x)
-        return z / (torch.sum(z, dim=dim, keepdim=True) + 1)
+        return z / (torch.sum(z, dim=2, keepdim=True) + 1)
 
-    def forward(self, x, attn_mask, return_attn=False):
-        if torch.any(torch.isnan(x)):
-            raise Exception('NaN encountered in SelfAttention.forward')
+    def forward(self, x, src_mask, distances):
+        """
+        Forward pass through Self-Attention layer
 
-        # We normalize the keys and queries as this helps desaturate softmax
+        :param x:           Input tensor of shape (batch_size, num_timesteps, d_model)
+        :param src_mask:    Attention mask of shape (batch_size, num_timesteps, num_timesteps)
+        :param distances:   Distance tensor of shape (batch_size, num_timesteps, num_timesteps)
+        :return:            Tensor of shape (batch_size, num_timesteps, d_model)
+        """
+        # We normalize keys and queries as this helps bound the attention
+        # coefficients and desaturate the softmax
         # See: https://aclanthology.org/2020.findings-emnlp.379.pdf
         q = F.normalize(self._Q(x), p=2.0, dim=2)
         k = F.normalize(self._K(x), p=2.0, dim=2)
-        v = self._V(x)
+        attn = torch.matmul(q, torch.transpose(k, 1, 2))
 
-        self_attn = torch.matmul(q, torch.transpose(k, 1, 2)) / self._dk
+        # Relative positional encoding using parameterized decay function
+        r = self._p1 * (torch.exp(-self._p0 * distances) - 1)  # r_ij ~ (-p1, 0]
+        attn = attn + r
 
-        # `attn_mask` can be used to mask out future positions and padding positions
-        if attn_mask is not None:
-            self_attn = self_attn.masked_fill(mask=attn_mask, value=-float('inf'))
+        # `src_mask` can be used to mask out future positions and padding
+        # positions in attention matrix
+        if src_mask is not None:
+            attn = attn.masked_fill(mask=src_mask, value=-float('inf'))
 
-        if return_attn:
-            return self_attn
-
-        return torch.bmm(self._softmax(self_attn, dim=2), v)
+        return torch.bmm(self._softmax(attn), self._V(x))
--- model/attention/nn/transformer_models.py
+++ model/attention/nn/transformer_models.py
@@ -1,77 +1,108 @@
 import torch
-import numpy as np
-from input_encoding import SinCosPositionalEncoding
+import torch.nn.functional as F
 from transformer_layers import TransformerEncoderLayer
 
 
 class CausalTransformer(torch.nn.Module):
-    """ Implementation of a Transformer (Vaswani et al., 2017)
-        with causal self-attention and relative positional encoding
-    """
-    def __init__(self, in_channels, out_channels, d_model=64, d_key=16, n_blocks=3, maxlen=256, device='cpu'):
+    def __init__(self, vocab_size, out_channels, d_model=64, d_key=32, n_blocks=2, n_heads=2, padding_value=0):
+        """
+        Transformer (Vaswani et al., 2017) with Causal Self-Attention
+
+        :param vocab_size:     Number of measurement types in dataset
+        :param out_channels:   Number of output channels
+        :param d_model:        Latent dimensions of the model (default: 64)
+        :param d_key:          Dimensions of queries/keys in self-attention computation (default: 32)
+        :param n_blocks:       Number of encoder blocks (default: 2)
+        :param n_heads:        Number of attention heads per block (default: 2)
+        :param padding_value:  Entry in vocab reserved for padding (default: 0)
+        """
         super(CausalTransformer, self).__init__()
         self.config = locals()
-        self._maxlen = maxlen
-        self._device = device
+        self._padding_value = padding_value
+        self._vocab_size = vocab_size
 
-        # Input encoding
-        self._input_encoding = torch.nn.Linear(in_channels, d_model)
-        self._positional_encoding = torch.nn.Linear(1, d_model)
+        # Input encoding network
+        self._input_encoding = torch.nn.Sequential(
+            torch.nn.Linear(vocab_size + 1, 96),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(96, d_model)
+        )
 
-        # Transformer layers
-        self._blocks = torch.nn.ModuleList()
-        for _ in range(n_blocks):
-            self._blocks.append(TransformerEncoderLayer(d_model=d_model, d_key=d_key))
-        self._feedforward = torch.nn.Linear(d_model, out_channels)
-
+        # Transformer blocks + FF layer
+        self._encoder_layers = torch.nn.ModuleList(
+            [TransformerEncoderLayer(d_model, n_heads, d_key) for _ in range(n_blocks)]
+        )
+        self._linear = torch.nn.Linear(d_model, out_channels)
         self._initialize()
-        self.to(device)
 
     def _initialize(self):
-        """ Initializes weights using Xavier initialization """
+        # Initialize model with Xavier initialization
         for p in self.parameters():
             if p.dim() > 1:
                 torch.nn.init.xavier_uniform_(p)
 
-    def _get_positional_encoding(self, x):
-        """ Compute positional encodings with a random offset (to combat position memorization) """
-        assert x.size(1) <= self._maxlen
+    @staticmethod
+    def _distance_matrix(t):
+        """
+        Computes relative distances of timesteps in x_pos for
+        relative positional encoding (RPE)
 
-        # Compute positions [0, ..., |x| - 1] + random offset
-        positions = torch.arange(x.size(1)).unsqueeze(0).unsqueeze(2).float()
-        offsets = torch.randint(low=0, high=self._maxlen - x.size(1), size=(x.size(0), 1, 1))
+        :return: Distance tensor of shape (batch_size, num_timesteps, num_timesteps)
+        """
+        return torch.abs(t.permute(0, 2, 1) - t).detach()
 
-        # Compute encoding as PositionalEncoding(Norm(positions + offset))
-        off_pos = (positions + offsets) / self._maxlen
-        return self._positional_encoding(off_pos).to(self._device)
+    @staticmethod
+    def _causal_mask(t):
+        """
+        Computes causal mask from timesteps in x_pos
 
-    def _get_causal_mask(self, n):
-        """ Upper triangular mask to mask out future positions """
-        return torch.triu(torch.ones(n, n), diagonal=1).unsqueeze(0).bool().to(self._device)
+        :return: Causal mask of shape (batch_size, num_timesteps, num_timesteps)
+        """
+        return (t.permute(0, 2, 1) - t) > 0
 
-    def forward(self, x, return_last=False):
-        """ Forward pass through transformer """
-        mask = self._get_causal_mask(x.size(1))
+    def _padding_mask(self, t):
+        """
+        Constructs padding mask from ids in x_type
 
-        y = self._input_encoding(x) + self._get_positional_encoding(x)
-        for block in self._blocks:
-            y = block(y, attn_mask=mask)
-        return self._feedforward(y[:, -1] if return_last else y)
+        :return: Padding mask of shape (batch_size, num_timesteps, num_timesteps)
+        """
+        return torch.transpose(t == self._padding_value, 1, 2)
 
-    def get_attention_matrices(self, x, softmax=False):
-        """ Yields attention matrices of shape (batches, length, length) """
-        matrices = []
-        with torch.no_grad():
-            mask = self._get_causal_mask(x.size(1))
+    def _encode_input(self, x_type, x_val):
+        """
+        Encodes sequence of (measurement_type, value) pairs into a latent vector
 
-            y = self._input_encoding(x) + self._get_positional_encoding(x)
-            for block in self._blocks:
-                # Extract attention matrix from layer
-                attn = block.self_attn(y, attn_mask=mask, return_attn=True).detach()
-                if softmax:
-                    attn = torch.softmax(attn, dim=2)
+        :param x_type:  Input tensor of shape (batch_size, num_timesteps, 1)
+        :param x_val:   Input tensor of shape (batch_size, num_timesteps, 1)
+        :return:        Tensor of shape (batch_size, num_timesteps, d_model)
+        """
+        # One-hot encode x_type (i.e. measurement types) and jointly model with x_val
+        x_type = F.one_hot(x_type[:, :, 0].long(), num_classes=self._vocab_size)
+        z = torch.concat([x_type, x_val], dim=2)
+        return self._input_encoding(z)
 
-                matrices.append(attn.numpy())
-                y = block(y, attn_mask=mask)
+    def forward(self, x, return_last=True):
+        """
+        Forward pass through transformer
 
-        return np.array(matrices)
+        :param x:            Tensor of shape (batch_size, num_timesteps, 3) where the features
+                             correspond to measurement_type, value and timestep respectively
+        :param return_last:  Whether to return the output of the final timestep only
+        :return:             Tensor of shape (batch_size, num_timesteps, out_channels)
+                             or (batch_size, out_channels) when `return_last=True`
+        """
+        x_type, x_val, x_pos = torch.split(x, 1, dim=2)
+
+        # Create combined causal/padding mask
+        padding_mask = self._padding_mask(x_type)
+        causal_mask = self._causal_mask(x_pos)
+        src_mask = torch.logical_or(padding_mask, causal_mask).detach()
+
+        # Compute RPE distances
+        distances = self._distance_matrix(x_pos).detach()
+
+        y = self._encode_input(x_type, x_val)
+        for layer in self._encoder_layers:
+            y = layer(y, src_mask=src_mask, distances=distances)
+        return self._linear(y[:, -1] if return_last else y)
+    
