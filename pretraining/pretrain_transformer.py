0fa46071c944811af6a187e6661aad87458a631d
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-11-07 23:16:34 +0100

    Add files via upload

--- pretraining/pretrain_transformer.py
+++ pretraining/pretrain_transformer.py
@@ -1,18 +1,18 @@
 import torch
 import numpy as np
 import pandas as pd
-from transformer_models import CausalTransformer
+from transformer_models import Transformer
 from behavior_cloning import fit_behavior_cloning
 from utils import count_parameters, load_data
 
 
 if __name__ == '__main__':
     # Training and validation dataset
-    train_df = load_data('../preprocessing/datasets/mimic-iii/attention_4h/mimic-iii_train.csv')
-    valid_df = load_data('../preprocessing/datasets/mimic-iii/attention_4h/mimic-iii_valid.csv')
+    train_df = load_data('../preprocessing/datasets/mimic-iii/attention_2h/mimic-iii_train.csv')
+    valid_df = load_data('../preprocessing/datasets/mimic-iii/attention_2h/mimic-iii_valid.csv')
 
-    # Set up encoder and classifier head
-    transformer = CausalTransformer(vocab_size=46, out_channels=96, d_model=64, n_heads=4, n_blocks=2, d_key=16)
+    # Set up encoder and classification head
+    transformer = Transformer(vocab_size=46, out_channels=96, d_model=64, n_heads=4, n_blocks=2, d_key=16)
     print('Transformer parameters:', count_parameters(transformer))
 
     classifier = torch.nn.Sequential(
@@ -29,7 +29,7 @@ if __name__ == '__main__':
                          valid_data=valid_df,
                          lrate=1e-3,
                          epochs=100,
-                         oversample_vaso=5000,   # Add additional 5000 trajectories with infrequent actions by oversampling
+                         oversample_vaso=6000,   # Add additional trajectories with infrequent actions by oversampling
                          batches_per_epoch=100,
                          truncate=256,
                          batch_size=8,
