7b06e458188478dca398fff99bbf9d124b8d5eb0
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-11-12 17:40:48 +0100

    soft confusion matrix

--- pretraining/plotting/confusion_matrix.py
+++ pretraining/plotting/confusion_matrix.py
@@ -10,7 +10,16 @@ from dataloader import DataLoader
 from utils import load_pretrained, to_numpy
 
 
-def predict_on_dataset(model, data_df, maxlen=256, batch_size=8, num_samples=200, seed=42):
+def soft_confusion_matrix(y_true, y_pred, labels, normalize):
+    out = np.zeros((len(labels), len(labels)), dtype=np.float64)
+    for i, j in zip(y_true, y_pred):
+        out[i] += j
+    if normalize:
+        out = out / np.sum(out, axis=1, keepdims=True)
+    return out
+
+
+def predict_on_dataset(model, data_df, maxlen=256, batch_size=8, num_samples=-1, seed=42, soft=False):
     """ Collects predictions of model on dataset """
     np.random.seed(seed)
 
@@ -22,17 +31,19 @@ def predict_on_dataset(model, data_df, maxlen=256, batch_size=8, num_samples=200
         y_pred, y_true = [], []
         total_samples = 0
 
-        with tqdm(desc='Collecting predictions') as pbar:
+        with tqdm(total=total_samples, desc='Collecting predictions') as pbar:
             for states, actions in dataloader.iterate(batch_size):
                 # Predict action class using model
-                action_preds = torch.argmax(model(states), dim=1)
+                model_pred = model(states)
+                action_preds = torch.softmax(model_pred, dim=1) if soft else torch.argmax(model_pred, dim=1)
                 y_pred.append(to_numpy(action_preds))
                 y_true.append(to_numpy(actions))
                 pbar.update(batch_size)
 
                 total_samples += batch_size
-                if total_samples > num_samples:
-                    break
+                if num_samples < 0 or total_samples < num_samples:
+                    continue
+                break
 
     # Concatenate collected predictions as vector
     y_true = np.concatenate(y_true, axis=0)
@@ -40,14 +51,24 @@ def predict_on_dataset(model, data_df, maxlen=256, batch_size=8, num_samples=200
     return y_true, y_pred
 
 
-def plot_confusion_matrix(model, data_df, maxlen, batch_size, num_samples):
+def plot_confusion_matrix(model, data_df, maxlen, batch_size, num_samples, soft=False):
     """ Plots confusion table of model predictions """
     # Collect predictions of model
     model.eval()
-    y_true, y_pred = predict_on_dataset(model, data_df, maxlen=maxlen, batch_size=batch_size, num_samples=num_samples)
+    y_true, y_pred = predict_on_dataset(
+        model=model,
+        data_df=data_df,
+        maxlen=maxlen,
+        batch_size=batch_size,
+        num_samples=num_samples,
+        soft=soft
+    )
 
     # Compute confusion matrix
-    conf_mat = confusion_matrix(y_true, y_pred, labels=np.arange(25), normalize='true')
+    if soft:
+        conf_mat = soft_confusion_matrix(y_true, y_pred, labels=np.arange(25), normalize='true')
+    else:
+        conf_mat = confusion_matrix(y_true, y_pred, labels=np.arange(25), normalize='true')
 
     plt.matshow(conf_mat)
     plt.title('Confusion table')
@@ -56,15 +77,19 @@ def plot_confusion_matrix(model, data_df, maxlen, batch_size, num_samples):
     plt.show()
 
     # Print per-class precision, recall and F1
-    print(classification_report(y_true, y_pred))
+    if not soft:
+        print(classification_report(y_true, y_pred))
 
 
 if __name__ == '__main__':
-    model = load_pretrained("../results/transformer_v2_pretraining_00000/classifier (8).pt")
-    data = pd.read_csv("../../preprocessing/datasets/mimic-iii/attention_2h/mimic-iii_valid.csv")
+    model = load_pretrained("../results/transformer_v2_pretraining_00000/classifier.pt")
+    data = pd.read_csv("../../preprocessing/datasets/mimic-iii/non_aggregated_1h/mimic-iii_valid.csv")
 
-    plot_confusion_matrix(model=model,
-                          data_df=data,
-                          maxlen=256,
-                          batch_size=8,
-                          num_samples=5000)
+    plot_confusion_matrix(
+        model=model,
+        data_df=data,
+        maxlen=256,
+        batch_size=8,
+        num_samples=5000,
+        soft=False
+    )
