b9c25123bf3dda1143d56625dff8f8a428df9a30
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-11-15 13:04:32 +0100

    Add files via upload

---
+++ pretraining/plotting/plot_positional_encodings.py
@@ -0,0 +1,77 @@
+import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_pretrained(path):
+    """ Load pretrained pytorch model from file
+    :param path:  Path to Transformer instance in pt format
+    :returns:     A PyTorch model
+    """
+    if not os.path.exists(path):
+        raise Exception('%s does not exist' % path)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = torch.load(path, map_location=device)
+    model.eval()
+    return model
+
+
+def get_positional_encoders(model):
+    """ Crudely extracts positional encoding modules
+    :param model: A Transformer instance
+    :returns:     Dict mapping (block, head) to a RelativePositionalEncoding instance
+    """
+    pos_encoders = dict()
+
+    # Extract positional encoders from each block and head
+    for i, block in model.get_submodule('_encoder_layers').named_children():
+        for j, self_attn_head in block.get_submodule('self_attn').get_submodule('_heads').named_children():
+            pos_encoding = self_attn_head.get_submodule('_pos_encoding')
+            pos_encoders[(int(i), int(j))] = self_attn_head.get_submodule('_pos_encoding')
+
+    # Extract positional encoders from fusion layer
+    for j, self_attn_head in model.get_submodule('_fusion').get_submodule('self_attn').get_submodule('_heads').named_children():
+        pos_encoding = self_attn_head.get_submodule('_pos_encoding')
+        pos_encoders[(int(i) + 1, int(j))] = self_attn_head.get_submodule('_pos_encoding')
+
+    return pos_encoders
+
+
+def visualize_rel_positions(pos_encoders, n_blocks, n_heads, max_dist=72, steps=1000):
+    """ Visualizes the relative positions matrix by feeding model a sequence
+        of relative positions and observing the encoder's response curve
+    :param pos_encoders:  Dict mapping (block, head) to a RelativePositionalEncoding instance
+    :param n_blocks:      Number of blocks in Transformer
+    :param n_heads:       Number of heads in Transformer
+    :param max_dist:      Maximum distance to look into the past from current observation (i.e. now - observation_time)
+    :param steps:         Number of points along range (set to high value)
+    """
+    # define rel pos vector with relative (absolute) distances in specified range
+    rel_pos = torch.linspace(0, max_dist, steps).unsqueeze(0).unsqueeze(0).unsqueeze(0)
+
+    plt.figure(figsize=(12, 8))
+    for block in range(n_blocks):
+        for head in range(n_heads):
+            pos_encoder = pos_encoders[(block, head)](rel_pos)
+
+            # observe response of positional encoder
+            response = -torch.squeeze(pos_encoder).detach().numpy() # pos encoding is subtracted from attention matrix!
+            times = -torch.squeeze(rel_pos).detach().numpy()        # bigger relative distance -> further in the past
+
+            # Plot response as a function of time
+            plt.subplot(n_blocks, n_heads, n_heads * block + head + 1)
+            plt.plot(times, response)
+            plt.title('block: %d - head: %d' % (block, head))
+            plt.xlabel('Time (h)')
+            plt.ylabel('Response')
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    model = load_pretrained("../results/transformer_v2_pretraining_00000/encoder.pt")
+    pos_encoders = get_positional_encoders(model)
+
+    visualize_rel_positions(pos_encoders, n_blocks=3, n_heads=4, max_dist=12, steps=1000)
