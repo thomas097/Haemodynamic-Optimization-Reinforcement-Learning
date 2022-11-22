ce7c669b9bdafa04d34f8e1f01b56244ea6505bd
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-11-22 22:44:23 +0100

    fixed paths

--- ope/physician_policy/fit_physician_policy.py
+++ ope/physician_policy/fit_physician_policy.py
@@ -3,15 +3,17 @@ import faiss
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from pathlib import Path
+from pathlib import Path, PurePath
 
 
 class FaissWeightedKNN:
     """ Fast implementation of k-Nearest Neighbors using faiss """
-    def __init__(self, weights, actions, k=5):
+    def __init__(self, weights, actions, k=5, nprobe=10, decay_rate=1.0):
         self._index = None
         self._actions = actions
         self._weights = weights[np.newaxis]
+        self._nprobe = nprobe
+        self._decay_rate = decay_rate   # to emphasize states that are closest
         self._y = None
         self._k = k
 
@@ -24,6 +26,7 @@ class FaissWeightedKNN:
     def fit(self, X, y):
         self._y = y
         self._index = faiss.IndexFlatL2(X.shape[1])
+        self._index.nprobe = self._nprobe
         self._index.add(self._apply_weights(X))
 
     def predict(self, X):
@@ -38,19 +41,16 @@ class FaissWeightedKNN:
         # Determine actions associated with states
         actions = self._y[indices]
 
-        # Convert distances to similarity
-        weights = np.exp(-distances)
+        # Convert distance to similarity
+        weights = 1 / (self._decay_rate * distances + 1)
 
         # Compute for each action the total sum of actions weighted by the distance of their respective states
         action_count = np.zeros((X.shape[0], len(self._actions)))
         for i, a in enumerate(self._actions):
             action_count[:, i] = np.sum(weights * (actions == a), axis=1)
 
-            # Add true action to it with a weight of 1 if given
             if true_action is not None:
-                action_count[:, i] += (true_action == a).flatten()
-
-        # TODO: force action chosen to be highest!
+                action_count[:, i] += (true_action == a)
 
         # Normalize to obtain probabilities
         return action_count / np.sum(action_count, axis=1, keepdims=True)
@@ -59,17 +59,16 @@ class FaissWeightedKNN:
 def get_feature_weights(feature_names, feature_weights):
     # Create vector of feature weights taking into account special feature weights
     weights = np.ones(len(feature_names))
-    for feat, weight in feature_weights.items():
-        if feat not in feature_names:
-            raise Exception('Feature %s not in feature_names' % feature_names)
-        weights[feature_names.index(feat)] = weight
+    for i, feat in enumerate(feature_names):
+        if feat in feature_weights:
+            weights[i] = feature_weights[feat]
     return weights
 
 
 def estimate_behavior_policy(train_set, weights, k=300):
     """ kNN-based behavior policy estimation for OPE. Please refer to (Raghu et al., 2018) for details.
 
-        train_set:     Path to preprocessed training set with states and chosen actions (by physisian)
+        train_set:     Path to preprocessed training set with states and chosen actions (by physician)
         weights:       Importance weights to assign to each state-space feature
 
         returns:       Fitted kNN model and list of chosen actions (i.e. train_set[action_col])
@@ -106,7 +105,7 @@ def evaluate_policy(policy, dataset, batch_size=128):
     action_probs = []
 
     # Process batch-wise to reduce overload
-    print('Processing %s\n' % dataset)
+    print('\nProcessing %s\n' % dataset)
     for i in tqdm(range(0, len(indices), batch_size)):
         index_batch = indices[i: i + batch_size]
 
@@ -123,33 +122,37 @@ def evaluate_policy(policy, dataset, batch_size=128):
 
 
 if __name__ == '__main__':
-    # Estimate behavior policy from training set
-    TRAIN_SET = '../../preprocessing/datasets/mimic-iii/roggeveen_4h_with_cv/mimic-iii_train.csv'
-    OUT_DIR = 'roggeveen_4h_with_cv'
+    # estimate behavior policy from training set
+    TRAIN_SET = '../../preprocessing/datasets/mimic-iii/aggregated_all_1h/mimic-iii_train.csv'
 
     # Evaluate policy's actions on train/valid/test sets
-    DATASETS = ['../../preprocessing/datasets/mimic-iii/roggeveen_4h_with_cv/mimic-iii_train.csv',
-                '../../preprocessing/datasets/mimic-iii/roggeveen_4h_with_cv/mimic-iii_valid.csv',
-                '../../preprocessing/datasets/mimic-iii/roggeveen_4h_with_cv/mimic-iii_test.csv']
+    DATASETS = ['../../preprocessing/datasets/mimic-iii/aggregated_all_1h/mimic-iii_train.csv',
+                '../../preprocessing/datasets/mimic-iii/aggregated_all_1h/mimic-iii_valid.csv',
+                '../../preprocessing/datasets/mimic-iii/aggregated_all_1h/mimic-iii_test.csv']
 
     # Assign certain features additional weight. Please refer to (Roggeveen et al., 2021).
-    # Remark: In the original work, `chloride` is also up-weighted, but not in the code (why?)
-    SPECIAL_WEIGHTS = {'x7': 2,   # age
-                       'x29': 2,  # lactate
-                       'x43': 2,  # pf_ratio
-                       'x3': 2,   # sofa_score
-                       'x4': 2,   # weight
-                       'x46': 2,  # total_urine_output
-                       'x12': 2,  # dias_bp
-                       'x11': 2,  # mean_bp
-                       'x1': 2}   # total_iv_fluid_prev
+    # Remark: In the original work and Raghu et al., `chloride` is said to be up-weighted, but not in the code (Why?)
+    SPECIAL_WEIGHTS = {
+        'x1': 2,   # total_iv_fluid_prev
+        'x3': 2,   # sofa_score
+        'x4': 2,   # weight
+        'x7': 2,   # age
+        'x11': 2,  # mean_bp
+        'x12': 2,  # dias_bp
+        'x17': 2,  # chloride
+        'x29': 2,  # lactate
+        'x43': 2,  # pf_ratio
+        'x46': 2,  # total_urine_output
+    }
     STATE_COLS = ['x%d' % i for i in range(48)]
 
     #######################
     #   Estimate Policy   #
     #######################
 
-    # Step 0. Create output directory
+    # Step 0. Create output directory, e.g. mimic-iii-aggregated_1h
+    parents = PurePath(TRAIN_SET).parents
+    OUT_DIR = '%s-%s' % (parents[1].name, parents[0].name)
     if not os.path.exists(OUT_DIR):
         os.makedirs(OUT_DIR)
 
@@ -161,8 +164,19 @@ if __name__ == '__main__':
     for dataset in DATASETS:
         action_probs = evaluate_policy(policy, dataset)
 
-        outfile = os.path.join(OUT_DIR, Path(dataset).stem)
-        action_probs.to_csv(outfile + '_behavior_policy.csv', index=False)
+        outfile = os.path.join(OUT_DIR, Path(dataset).stem + '_behavior_policy.csv')
+        action_probs.to_csv(outfile, index=False)
+
+    # Sanity Check: Is kNN model predictive of action?
+    from sklearn.metrics import f1_score
+
+    chosen_actions = pd.read_csv(DATASETS[1]).action
+    action_probs = evaluate_policy(policy, DATASETS[1]).filter(regex='\d+').values
+    predicted_actions = np.argmax(action_probs, axis=1)
+
+    print('f1:', f1_score(chosen_actions, predicted_actions, average='macro'))
+
+
 
 
 
