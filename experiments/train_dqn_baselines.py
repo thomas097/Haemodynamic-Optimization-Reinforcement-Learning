d27254a0595692dabfef6b3ec78d081377a99f0d
ThBellucci <40698255+thomas097@users.noreply.github.com>
2023-02-08 12:50:36 +0100

    Added command line interface for reproducibility

--- experiments/train_dqn_baselines.py
+++ experiments/train_dqn_baselines.py
@@ -1,4 +1,14 @@
+# to allow running from terminal without pyCharm :(
+import sys
+
+sys.path.append('../ope')
+sys.path.append('../models/q_learning')
+sys.path.append('../models/attention/nn')
+sys.path.append('../models/ckconv/nn')
+sys.path.append('../models')
+
 import torch
+import argparse
 import pandas as pd
 from q_learning import DQN, fit_double_dqn
 from baseline_encoders import *
@@ -6,39 +16,49 @@ from utils import load_data, count_parameters, OPECallback, add_intermediate_rew
 
 
 if __name__ == '__main__':
-    # Choose history-encoding model and output dimensions
-    METHOD = 'last_state'  # handcrafted state-space baseline
-    OUT_CHANNELS = 96
-    HIDDEN_CHANNELS = 32
+    parser = argparse.ArgumentParser(description='Trains policy with pretrained encoder')
+    parser.add_argument('-d', "--dataset", type=str, default='amsterdam-umc-db')
+    parser.add_argument('-m', "--baseline", type=str, default='last_state')
+    parser.add_argument('-o', "--out_dims", type=int, default=96)
+    parser.add_argument('-a', "--alpha", type=float, default=1e-4)
+    parser.add_argument('-g', "--gamma", type=float, default=0.95)
+    parser.add_argument('-l', "--tau", type=float, default=1e-4)
+    parser.add_argument('-e', "--episodes", type=int, default=200000)
+    parser.add_argument('-k', "--eval_after", type=int, default=5000)
+    parser.add_argument('-z', "--freeze_encoder", type=bool, default=False)
+    parser.add_argument('-w', "--replay_alpha", type=float, default=0.6)
+    parser.add_argument('-q', "--replay_beta", type=float, default=0.9)
+    parser.add_argument('-b', "--batch_size", type=int, default=32)
+    args = vars(parser.parse_args())
+    print('Running with args:', args)
 
-    # Training and validation sets
-    behavior_policy_file = '../ope/physician_policy/amsterdam-umc-db_aggregated_full_cohort_2h_mlp/valid_behavior_policy.csv'
-    train_df = load_data('../preprocessing/datasets/amsterdam-umc-db/aggregated_full_cohort_2h/train.csv')
-    valid_df = load_data('../preprocessing/datasets/amsterdam-umc-db/aggregated_full_cohort_2h/valid.csv')
+    behavior_policy_file = '../ope/physician_policy/%s_aggregated_full_cohort_2h_mlp/valid_behavior_policy.csv' % args['dataset']
+    train_df = load_data('../preprocessing/datasets/%s/aggregated_full_cohort_2h/train.csv' % args['dataset'])
+    valid_df = load_data('../preprocessing/datasets/%s/aggregated_full_cohort_2h/valid.csv' % args['dataset'])
 
-    # Add intermediate rewards based on MAP
-    train_df.reward = add_intermediate_reward(train_df, scale=1)
-    valid_df.reward = add_intermediate_reward(valid_df, scale=1)
+    # Add intermediate rewards based on MAP and lactate
+    train_df.reward = add_intermediate_reward(train_df)
+    valid_df.reward = add_intermediate_reward(valid_df)
 
     in_channels = train_df.filter(regex='x\d+').shape[1]
 
-    if METHOD == 'last_state':
-        encoder = LastState(in_channels, OUT_CHANNELS)
-    elif METHOD == 'concat-2':
-        encoder = StateConcatenation(in_channels, OUT_CHANNELS, k=2)
-    elif METHOD == 'concat-3':
-        encoder = StateConcatenation(in_channels, OUT_CHANNELS, k=3)
-    elif METHOD == 'tcn':
-        encoder = TCN((in_channels, HIDDEN_CHANNELS, OUT_CHANNELS), kernel_sizes=(18, 3), dilations=(2, 1))
-    elif METHOD == 'lstm':
-        encoder = LSTM(in_channels, OUT_CHANNELS)
-    elif METHOD == 'gru':
-        encoder = GRU(in_channels, OUT_CHANNELS)
+    if args['baseline'] == 'last_state':
+        encoder = LastState(in_channels, args['out_dims'])
+    elif args['baseline'] == 'concat-2':
+        encoder = StateConcatenation(in_channels, args['out_dims'], k=2)
+    elif args['baseline'] == 'concat-3':
+        encoder = StateConcatenation(in_channels, args['out_dims'], k=3)
+    elif args['baseline'] == 'tcn':
+        encoder = TCN((in_channels, 64, args['out_dims']), kernel_sizes=(18, 3), dilations=(2, 1))
+    elif args['baseline'] == 'lstm':
+        encoder = LSTM(in_channels, args['out_dims'])
+    elif args['baseline'] == 'gru':
+        encoder = GRU(in_channels, args['out_dims'])
     else:
-        raise Exception('Method %s not recognized' % METHOD)
-    print('%s parameters: %d' % (METHOD, count_parameters(encoder)))
+        raise Exception('Method %s not recognized' % args['baseline'])
+    print('%s parameters: %d' % (args['baseline'], count_parameters(encoder)))
 
-    dqn = DQN(state_dim=OUT_CHANNELS, hidden_dims=(96, 96,), num_actions=25, disallowed_actions=(1, 2, 3, 4))
+    dqn = DQN(state_dim=args['out_dims'], hidden_dims=(96, 96,), num_actions=25, disallowed_actions=(1, 2, 3, 4))
     print('DQN parameters:  ', count_parameters(dqn))
 
     # intermittently evaluate policy
@@ -46,21 +66,22 @@ if __name__ == '__main__':
 
     # Fit!
     fit_double_dqn(
-        experiment='results/%s_experiment' % METHOD,
+        experiment='results/%s/%s_experiment' % (args['dataset'], args['baseline']),
         policy=dqn,
         encoder=encoder,
         dataset=train_df,
-        lrate=1e-4,
-        gamma=0.95,
-        tau=1e-4,
+        lrate=args['alpha'],
+        gamma=args['gamma'],
+        tau=args['tau'],
         lambda_reward=1,
-        num_episodes=200000,
-        batch_size=32,
+        num_episodes=args['episodes'],
+        batch_size=args['batch_size'],
+        freeze_encoder=args['freeze_encoder'],  # set freeze_encoder=True to disable fine-tuning!
         eval_func=callback,
-        eval_after=5000,
+        eval_after=args['eval_after'],
         scheduler_gamma=0.95,
-        replay_alpha=0.6,
-        replay_beta=0.9,
+        replay_alpha=args['replay_alpha'],
+        replay_beta=args['replay_beta'],
         step_scheduler_after=10000,
         min_max_reward=(-27, 10),
         save_on=None
--- experiments/train_dqn_pretrained.py
+++ experiments/train_dqn_pretrained.py
@@ -1,44 +1,70 @@
+# to allow running from terminal without pyCharm :(
+import sys
+
+sys.path.append('../ope')
+sys.path.append('../models/q_learning')
+sys.path.append('../models/attention/nn')
+sys.path.append('../models/ckconv/nn')
+sys.path.append('../models')
+
 import torch
+import argparse
 import pandas as pd
 from q_learning import DQN, fit_double_dqn
 from utils import load_data, load_pretrained, count_parameters, OPECallback, add_intermediate_reward
 
 
 if __name__ == '__main__':
-    behavior_policy_file = '../ope/physician_policy/amsterdam-umc-db_aggregated_full_cohort_2h_mlp/valid_behavior_policy.csv'
-    train_df = load_data('../preprocessing/datasets/amsterdam-umc-db/aggregated_full_cohort_2h/train.csv')
-    valid_df = load_data('../preprocessing/datasets/amsterdam-umc-db/aggregated_full_cohort_2h/valid.csv')
+    parser = argparse.ArgumentParser(description='Trains policy with pretrained encoder')
+    parser.add_argument('-d', "--dataset", type=str, default='amsterdam-umc-db')
+    parser.add_argument('-m', "--model", type=str, default='transformer_mt')
+    parser.add_argument('-o', "--out_dims", type=int, default=96)
+    parser.add_argument('-a', "--alpha", type=float, default=1e-4)
+    parser.add_argument('-g', "--gamma", type=float, default=0.95)
+    parser.add_argument('-l', "--tau", type=float, default=1e-4)
+    parser.add_argument('-e', "--episodes", type=int, default=200000)
+    parser.add_argument('-k', "--eval_after", type=int, default=5000)
+    parser.add_argument('-z', "--freeze_encoder", type=bool, default=False)
+    parser.add_argument('-w', "--replay_alpha", type=float, default=0.6)
+    parser.add_argument('-q', "--replay_beta", type=float, default=0.9)
+    parser.add_argument('-b', "--batch_size", type=int, default=32)
+    args = vars(parser.parse_args())
+    print('Running with args:', args)
 
-    # Add intermediate rewards based on MAP
+    behavior_policy_file = '../ope/physician_policy/%s_aggregated_full_cohort_2h_mlp/valid_behavior_policy.csv' % args['dataset']
+    train_df = load_data('../preprocessing/datasets/%s/aggregated_full_cohort_2h/train.csv' % args['dataset'])
+    valid_df = load_data('../preprocessing/datasets/%s/aggregated_full_cohort_2h/valid.csv' % args['dataset'])
+
+    # Add intermediate rewards based on MAP and lactate
     train_df.reward = add_intermediate_reward(train_df)
     valid_df.reward = add_intermediate_reward(valid_df)
 
-    encoder = load_pretrained('../pretraining/results/amsterdam-umc-db/transformer_mt_pretraining_00000/encoder.pt')
+    encoder = load_pretrained('../pretraining/results/%s/%s_pretraining_00000/encoder.pt' % (args['dataset'], args['model']))
     print('Encoder params:', count_parameters(encoder))
 
-    dqn = DQN(state_dim=96, hidden_dims=(96, 96), num_actions=25, disallowed_actions=(1, 2, 3, 4))
+    dqn = DQN(state_dim=args['out_dims'], hidden_dims=(96, 96), num_actions=25, disallowed_actions=(1, 2, 3, 4))
     print('DQN params:    ', count_parameters(dqn))
 
     # intermittently evaluate model
     callback = OPECallback(behavior_policy_file=behavior_policy_file, valid_data=valid_df)
 
     fit_double_dqn(
-        experiment='results/amsterdam-umc-db/transformer_mt_experiment',
+        experiment='results/%s/%s_experiment' % (args['dataset'], args['model']),
         policy=dqn,
         encoder=encoder,
         dataset=train_df,
-        lrate=1e-4,
-        gamma=0.95,
-        tau=1e-4,
+        lrate=args['alpha'],
+        gamma=args['gamma'],
+        tau=args['tau'],
         lambda_reward=1,
-        num_episodes=200000,
-        batch_size=32,
-        freeze_encoder=False, # set freeze_encoder=True to disable fine-tuning!
+        num_episodes=args['episodes'],
+        batch_size=args['batch_size'],
+        freeze_encoder=args['freeze_encoder'], # set freeze_encoder=True to disable fine-tuning!
         eval_func=callback,
-        eval_after=5000,
+        eval_after=args['eval_after'],
         scheduler_gamma=0.95,
-        replay_alpha=0.6,
-        replay_beta=0.9,
+        replay_alpha=args['replay_alpha'],
+        replay_beta=args['replay_beta'],
         step_scheduler_after=10000,
         min_max_reward=(-27, 10),
         save_on=None
