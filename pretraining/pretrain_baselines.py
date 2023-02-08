f9946cc5359d31ad502f7504dc2082e51b49c705
ThBellucci <40698255+thomas097@users.noreply.github.com>
2023-02-08 12:16:31 +0100

    added command line interface to pretrain models

--- pretraining/pretrain_baselines.py
+++ pretraining/pretrain_baselines.py
@@ -1,4 +1,14 @@
+# to allow running from terminal without pyCharm :(
+import sys
+
+sys.path.append('../ope')
+sys.path.append('../models/q_learning')
+sys.path.append('../models/attention/nn')
+sys.path.append('../models/ckconv/nn')
+sys.path.append('../models')
+
 import torch
+import argparse
 import numpy as np
 import pandas as pd
 from baseline_encoders import *
@@ -19,30 +29,43 @@ class MeanEncoder(torch.nn.Module):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Trains baseline LSTM encoder on desired pretraining task')
+    parser.add_argument('-d', "--dataset", type=str, default='amsterdam-umc-db')
+    parser.add_argument('-t', "--task", type=str, default='mt')
+    parser.add_argument('-o', "--out_dims", type=int, default=96)
+    parser.add_argument('-l', "--lrate", type=float, default=5e-4)
+    parser.add_argument('-e', "--epochs", type=int, default=200)
+    parser.add_argument('-z', "--batches_per_epoch", type=int, default=500)
+    parser.add_argument('-w', "--warmup", type=int, default=50)
+    parser.add_argument('-b', "--batch_size", type=int, default=32)
+    args = vars(parser.parse_args())
+    print('Running with args:', args)
+
     # Training and validation dataset
-    train_df = pd.read_csv('../preprocessing/datasets/amsterdam-umc-db/aggregated_full_cohort_2h/train.csv')
-    valid_df = pd.read_csv('../preprocessing/datasets/amsterdam-umc-db/aggregated_full_cohort_2h/valid.csv')
+    train_df = pd.read_csv('../preprocessing/datasets/%s/aggregated_full_cohort_2h/train.csv' % args['dataset'])
+    valid_df = pd.read_csv('../preprocessing/datasets/%s/aggregated_full_cohort_2h/valid.csv' % args['dataset'])
 
     in_channels = train_df.filter(regex='x\d+').shape[1]
-    out_channels = 32
+    out_channels = args['out_dims']
 
-    # Set up encoder and decoder head
-    encoder = MeanEncoder(out_channels) # Replace with encoder of choice, e.g. LastState(in_channels, out_channels)
+    # Set up encoder
+    lstm = LSTM(state_dim=in_channels, hidden_dims=out_channels) # Replace with encoder of choice
 
     # Train!
     fit_multi_task(
-        task='mt',
-        experiment='results/mean_encoder_pretraining',
-        encoder=encoder,
+        task=args['task'],
+        experiment='results/%s/transformer_%s_pretraining' % (args['dataset'], args['task']),
+        encoder=lstm,
         in_channels=in_channels,
         out_channels=out_channels,
         train_data=train_df,
         valid_data=valid_df,
         mask_missing=True,
-        lrate=5e-4,
-        epochs=200,
-        batches_per_epoch=500,
-        warmup=50,
-        batch_size=8,
+        lrate=args['lrate'],
+        epochs=args['epochs'],
+        batches_per_epoch=args['batches_per_epoch'],
+        warmup=args['warmup'],
+        truncate=256,
+        batch_size=args['batch_size'],
         save_on='valid_mse'
     )
--- pretraining/pretrain_transformer.py
+++ pretraining/pretrain_transformer.py
@@ -1,4 +1,14 @@
+# to allow running from terminal without pyCharm :(
+import sys
+
+sys.path.append('../ope')
+sys.path.append('../models/q_learning')
+sys.path.append('../models/attention/nn')
+sys.path.append('../models/ckconv/nn')
+sys.path.append('../models')
+
 import torch
+import argparse
 import numpy as np
 import pandas as pd
 from vanilla_transformer import Transformer
@@ -7,12 +17,24 @@ from utils import count_parameters, load_pretrained
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Trains transformer encoder on desired pretraining task')
+    parser.add_argument('-d', "--dataset", type=str, default='amsterdam-umc-db')
+    parser.add_argument('-t', "--task", type=str, default='mt')
+    parser.add_argument('-o', "--out_dims", type=int, default=96)
+    parser.add_argument('-l', "--lrate", type=float, default=5e-4)
+    parser.add_argument('-e', "--epochs", type=int, default=200)
+    parser.add_argument('-z', "--batches_per_epoch", type=int, default=500)
+    parser.add_argument('-w', "--warmup", type=int, default=50)
+    parser.add_argument('-b', "--batch_size", type=int, default=32)
+    args = vars(parser.parse_args())
+    print('Running with args:', args)
+
     # Training and validation dataset
-    train_df = pd.read_csv('../preprocessing/datasets/amsterdam-umc-db/aggregated_full_cohort_2h/train.csv')
-    valid_df = pd.read_csv('../preprocessing/datasets/amsterdam-umc-db/aggregated_full_cohort_2h/valid.csv')
+    train_df = pd.read_csv('../preprocessing/datasets/%s/aggregated_full_cohort_2h/train.csv' % args['dataset'])
+    valid_df = pd.read_csv('../preprocessing/datasets/%s/aggregated_full_cohort_2h/valid.csv' % args['dataset'])
 
     in_channels = train_df.filter(regex='x\d+').shape[1]
-    out_channels = 96
+    out_channels = args['out_dims']
 
     # Set up encoder
     transformer = Transformer(
@@ -28,19 +50,19 @@ if __name__ == '__main__':
 
     # Train!
     fit_multi_task(
-        task='mt',
-        experiment='results/transformer_mt_pretraining',
+        task=args['task'],
+        experiment='results/%s/transformer_%s_pretraining' % (args['dataset'], args['task']),
         encoder=transformer,
         in_channels=in_channels,
         out_channels=out_channels,
         train_data=train_df,
         valid_data=valid_df,
         mask_missing=True,
-        lrate=5e-4,
-        epochs=200,
-        batches_per_epoch=500,
-        warmup=50,
+        lrate=args['lrate'],
+        epochs=args['epochs'],
+        batches_per_epoch=args['batches_per_epoch'],
+        warmup=args['warmup'],
         truncate=256,
-        batch_size=8,
+        batch_size=args['batch_size'],
         save_on='valid_mse'
     )
